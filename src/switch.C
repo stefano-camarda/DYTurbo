6a70bbc56bf042dedab189d44867bd98125df820
Stefano <stefano.camarda@cern.ch>
2015-10-10 13:37:40 +0200

    no root dependence

--- src/switch.C
+++ src/switch.C
@@ -1,7 +1,6 @@
 #include "switch.h"
 
 #include <math.h>
-#include <TMath.h>
 
 double switching::k = 3./4.;
 double switching::cutoff = 0.01;
@@ -26,7 +25,7 @@ double switching::swtch(double qt, double m)
   if (qt >= m/2)     swtch=exp(-pow(2*(m/2-qt),2)/pow((m/2),2));      // GAUSS SWITCH FASTER
   if (qt >= m/2)     swtch=exp(((m*m)/4-(qt*qt)) / (m*m) );           // EXP SWITCH
   if (qt >= m/2)     swtch=exp(((m*m)/4-(qt*qt))/(m*m)*4);            // EXP SWITCH FASTER
-  if (qt >= m/2)     swtch=(cos(TMath::Pi()/50*(qt-45.6))+1)/2;// COS SWITCH
+  if (qt >= m/2)     swtch=(cos(M_PI/50*(qt-45.6))+1)/2;// COS SWITCH
   if (qt >= 95.6)    swtch=0;   
   */
 
