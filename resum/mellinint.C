368a20339a9010a6d4b71e7597204b3a2479523e
Stefano Camarda <stefano.camarda@cern.ch>
2016-03-31 10:46:57 +0200

    fixed name of ddb and ubu mesq

--- resum/mellinint.C
+++ resum/mellinint.C
@@ -136,24 +136,24 @@ void mellinint::pdf_mesq_expy(int i1, int i2, int sign)
   if (opts.nproc == 3)
     {
       complex <double> mesq_uub = mesq::mesqij_expy[mesq::index(0,i1,i2,sign)];
-      complex <double> mesq_ddb = mesq::mesqij_expy[mesq::index(1,i1,i2,sign)];
-      complex <double> mesq_ubu = mesq::mesqij_expy[mesq::index(2,i1,i2,sign)];
+      complex <double> mesq_ubu = mesq::mesqij_expy[mesq::index(1,i1,i2,sign)];
+      complex <double> mesq_ddb = mesq::mesqij_expy[mesq::index(2,i1,i2,sign)];
       complex <double> mesq_dbd = mesq::mesqij_expy[mesq::index(3,i1,i2,sign)];
 
       //NLL part
       QGN_1 = fn1[g]*((fn2[ub]+fn2[cb])*mesq_uub
-		      + (fn2[db]+fn2[sb]+fn2[bb])*mesq_ubu
-		      + (fn2[u]+fn2[c])*mesq_ddb
+		      + (fn2[db]+fn2[sb]+fn2[bb])*mesq_ddb
+		      + (fn2[u]+fn2[c])*mesq_ubu
 		      + (fn2[d]+fn2[s]+fn2[b])*mesq_dbd);
 
       QGN_2 = fn2[g]*((fn1[u]+fn1[c])*mesq_uub
-		      + (fn1[d]+fn1[s]+fn1[b])*mesq_ubu
-		      + (fn1[ub]+fn1[cb])*mesq_ddb
+		      + (fn1[d]+fn1[s]+fn1[b])*mesq_ddb
+		      + (fn1[ub]+fn1[cb])*mesq_ubu
 		      + (fn1[db]+fn1[sb]+fn1[bb])*mesq_dbd);
 
       QQBN_1 = (fn1[u]*fn2[ub]+fn1[c]*fn2[cb])*mesq_uub
-	+ (fn1[d]*fn2[db]+fn1[s]*fn2[sb]+fn1[b]*fn2[bb])*mesq_ubu
-	+ (fn1[ub]*fn2[u]+fn1[cb]*fn2[c])*mesq_ddb
+	+ (fn1[d]*fn2[db]+fn1[s]*fn2[sb]+fn1[b]*fn2[bb])*mesq_ddb
+	+ (fn1[ub]*fn2[u]+fn1[cb]*fn2[c])*mesq_ubu
 	+ (fn1[db]*fn2[d]+fn1[sb]*fn2[s]+fn1[bb]*fn2[b])*mesq_dbd;
 
       //NNLL
@@ -161,176 +161,176 @@ void mellinint::pdf_mesq_expy(int i1, int i2, int sign)
 	{
 	  GGN = fn1[g]*fn2[g]*
 	    (2.*mesq_uub
-	     + 3.*mesq_ubu
-	     + 2.*mesq_ddb
+	     + 3.*mesq_ddb
+	     + 2.*mesq_ubu
 	     + 3.*mesq_dbd);
             
 	  QQBN_2 = (fn1[u]*fn2[u]+fn1[c]*fn2[c])*mesq_uub
-	    + (fn1[d]*fn2[d]+fn1[s]*fn2[s]+fn1[b]*fn2[b])*mesq_ubu
-	    + (fn1[ub]*fn2[ub]+fn1[cb]*fn2[cb])*mesq_ddb
+	    + (fn1[d]*fn2[d]+fn1[s]*fn2[s]+fn1[b]*fn2[b])*mesq_ddb
+	    + (fn1[ub]*fn2[ub]+fn1[cb]*fn2[cb])*mesq_ubu
 	    + (fn1[db]*fn2[db]+fn1[sb]*fn2[sb]+fn1[bb]*fn2[bb])*mesq_dbd;
 
-	  QQBN_3 = fn1[u]*(fn2[db]*mesq_ubu
-			   +fn2[sb]*mesq_ubu
+	  QQBN_3 = fn1[u]*(fn2[db]*mesq_ddb
+			   +fn2[sb]*mesq_ddb
 			   +fn2[cb]*mesq_uub
-			   +fn2[bb]*mesq_ubu
+			   +fn2[bb]*mesq_ddb
 			   +fn2[d]*mesq_dbd
 			   +fn2[s]*mesq_dbd
-			   +fn2[c]*mesq_ddb
+			   +fn2[c]*mesq_ubu
 			   +fn2[b]*mesq_dbd)
 	    + fn1[d]*(fn2[ub]*mesq_uub
-		      +fn2[sb]*mesq_ubu
+		      +fn2[sb]*mesq_ddb
 		      +fn2[cb]*mesq_uub
-		      +fn2[bb]*mesq_ubu
-		      +fn2[u]*mesq_ddb
+		      +fn2[bb]*mesq_ddb
+		      +fn2[u]*mesq_ubu
 		      +fn2[s]*mesq_dbd
-		      +fn2[c]*mesq_ddb
+		      +fn2[c]*mesq_ubu
 		      +fn2[b]*mesq_dbd)
 	    + fn1[s]*(fn2[ub]*mesq_uub
-		      +fn2[db]*mesq_ubu
+		      +fn2[db]*mesq_ddb
 		      +fn2[cb]*mesq_uub
-		      +fn2[bb]*mesq_ubu
-		      +fn2[u]*mesq_ddb
+		      +fn2[bb]*mesq_ddb
+		      +fn2[u]*mesq_ubu
 		      +fn2[d]*mesq_dbd
-		      +fn2[c]*mesq_ddb
+		      +fn2[c]*mesq_ubu
 		      +fn2[b]*mesq_dbd)
 	    + fn1[c]*(fn2[ub]*mesq_uub
-		      +fn2[db]*mesq_ubu
-		      +fn2[sb]*mesq_ubu
-		      +fn2[bb]*mesq_ubu
-		      +fn2[u]*mesq_ddb
+		      +fn2[db]*mesq_ddb
+		      +fn2[sb]*mesq_ddb
+		      +fn2[bb]*mesq_ddb
+		      +fn2[u]*mesq_ubu
 		      +fn2[d]*mesq_dbd
 		      +fn2[s]*mesq_dbd
 		      +fn2[b]*mesq_dbd)
 	    + fn1[b]*(fn2[ub]*mesq_uub
-		      +fn2[db]*mesq_ubu
-		      +fn2[sb]*mesq_ubu
+		      +fn2[db]*mesq_ddb
+		      +fn2[sb]*mesq_ddb
 		      +fn2[cb]*mesq_uub
-		      +fn2[u]*mesq_ddb
+		      +fn2[u]*mesq_ubu
 		      +fn2[d]*mesq_dbd
 		      +fn2[s]*mesq_dbd
-		      +fn2[c]*mesq_ddb)
-	    + fn1[ub]*(fn2[db]*mesq_ubu
-		       +fn2[sb]*mesq_ubu
+		      +fn2[c]*mesq_ubu)
+	    + fn1[ub]*(fn2[db]*mesq_ddb
+		       +fn2[sb]*mesq_ddb
 		       +fn2[cb]*mesq_uub
-		       +fn2[bb]*mesq_ubu
+		       +fn2[bb]*mesq_ddb
 		       +fn2[d]*mesq_dbd
 		       +fn2[s]*mesq_dbd
-		       +fn2[c]*mesq_ddb
+		       +fn2[c]*mesq_ubu
 		       +fn2[b]*mesq_dbd)
 	    + fn1[db]*(fn2[ub]*mesq_uub
-		       +fn2[sb]*mesq_ubu
+		       +fn2[sb]*mesq_ddb
 		       +fn2[cb]*mesq_uub
-		       +fn2[bb]*mesq_ubu
-		       +fn2[u]*mesq_ddb
+		       +fn2[bb]*mesq_ddb
+		       +fn2[u]*mesq_ubu
 		       +fn2[s]*mesq_dbd
-		       +fn2[c]*mesq_ddb
+		       +fn2[c]*mesq_ubu
 		       +fn2[b]*mesq_dbd)
 	    + fn1[sb]*(fn2[ub]*mesq_uub
-		       +fn2[db]*mesq_ubu
+		       +fn2[db]*mesq_ddb
 		       +fn2[cb]*mesq_uub
-		       +fn2[bb]*mesq_ubu
-		       +fn2[u]*mesq_ddb
+		       +fn2[bb]*mesq_ddb
+		       +fn2[u]*mesq_ubu
 		       +fn2[d]*mesq_dbd
-		       +fn2[c]*mesq_ddb
+		       +fn2[c]*mesq_ubu
 		       +fn2[b]*mesq_dbd)
 	    + fn1[cb]*(fn2[ub]*mesq_uub
-		       +fn2[db]*mesq_ubu
-		       +fn2[sb]*mesq_ubu
-		       +fn2[bb]*mesq_ubu
-		       +fn2[u]*mesq_ddb
+		       +fn2[db]*mesq_ddb
+		       +fn2[sb]*mesq_ddb
+		       +fn2[bb]*mesq_ddb
+		       +fn2[u]*mesq_ubu
 		       +fn2[d]*mesq_dbd
 		       +fn2[s]*mesq_dbd
 		       +fn2[b]*mesq_dbd)
 	    + fn1[bb]*(fn2[ub]*mesq_uub
-		       +fn2[db]*mesq_ubu
-		       +fn2[sb]*mesq_ubu
+		       +fn2[db]*mesq_ddb
+		       +fn2[sb]*mesq_ddb
 		       +fn2[cb]*mesq_uub
-		       +fn2[u]*mesq_ddb
+		       +fn2[u]*mesq_ubu
 		       +fn2[d]*mesq_dbd
 		       +fn2[s]*mesq_dbd
-		       +fn2[c]*mesq_ddb);
+		       +fn2[c]*mesq_ubu);
 
-	  QQBN_4 = fn2[u]*(fn1[d]*mesq_ubu
-			   +fn1[s]*mesq_ubu
+	  QQBN_4 = fn2[u]*(fn1[d]*mesq_ddb
+			   +fn1[s]*mesq_ddb
 			   +fn1[c]*mesq_uub
-			   +fn1[b]*mesq_ubu
+			   +fn1[b]*mesq_ddb
 			   +fn1[db]*mesq_dbd
 			   +fn1[sb]*mesq_dbd
-			   +fn1[cb]*mesq_ddb
+			   +fn1[cb]*mesq_ubu
 			   +fn1[bb]*mesq_dbd)
 	    + fn2[d]* (fn1[u]*mesq_uub
-		       +fn1[s]*mesq_ubu
+		       +fn1[s]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[sb]*mesq_dbd
-		       +fn1[cb]*mesq_ddb
+		       +fn1[cb]*mesq_ubu
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[s]* (fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
+		       +fn1[d]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
-		       +fn1[cb]*mesq_ddb
+		       +fn1[cb]*mesq_ubu
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[c]* (fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
-		       +fn1[s]*mesq_ubu
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[d]*mesq_ddb
+		       +fn1[s]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
 		       +fn1[sb]*mesq_dbd
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[b]* (fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
-		       +fn1[s]*mesq_ubu
+		       +fn1[d]*mesq_ddb
+		       +fn1[s]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[ub]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
 		       +fn1[sb]*mesq_dbd
-		       +fn1[cb]*mesq_ddb)
-	    + fn2[ub]*(fn1[d]*mesq_ubu
-		       +fn1[s]*mesq_ubu
+		       +fn1[cb]*mesq_ubu)
+	    + fn2[ub]*(fn1[d]*mesq_ddb
+		       +fn1[s]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[b]*mesq_ubu
+		       +fn1[b]*mesq_ddb
 		       +fn1[db]*mesq_dbd
 		       +fn1[sb]*mesq_dbd
-		       +fn1[cb]*mesq_ddb
+		       +fn1[cb]*mesq_ubu
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[db]*(fn1[u]*mesq_uub
-		       +fn1[s]*mesq_ubu
+		       +fn1[s]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[sb]*mesq_dbd
-		       +fn1[cb]*mesq_ddb
+		       +fn1[cb]*mesq_ubu
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[sb]*(fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
+		       +fn1[d]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
-		       +fn1[cb]*mesq_ddb
+		       +fn1[cb]*mesq_ubu
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[cb]*(fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
-		       +fn1[s]*mesq_ubu
-		       +fn1[b]*mesq_ubu
-		       +fn1[ub]*mesq_ddb
+		       +fn1[d]*mesq_ddb
+		       +fn1[s]*mesq_ddb
+		       +fn1[b]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
 		       +fn1[sb]*mesq_dbd
 		       +fn1[bb]*mesq_dbd)
 	    + fn2[bb]*(fn1[u]*mesq_uub
-		       +fn1[d]*mesq_ubu
-		       +fn1[s]*mesq_ubu
+		       +fn1[d]*mesq_ddb
+		       +fn1[s]*mesq_ddb
 		       +fn1[c]*mesq_uub
-		       +fn1[ub]*mesq_ddb
+		       +fn1[ub]*mesq_ubu
 		       +fn1[db]*mesq_dbd
 		       +fn1[sb]*mesq_dbd
-		       +fn1[cb]*mesq_ddb);
+		       +fn1[cb]*mesq_ubu);
 	}
     }
   if (opts.nproc == 1)
