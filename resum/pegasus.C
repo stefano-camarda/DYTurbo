7d1693f6ce049343addcf04a88054f0d52b068b2
Stefano Camarda <stefano.camarda@cern.ch>
2016-04-05 16:54:12 +0200

    pegasus interface

---
+++ resum/pegasus.C
@@ -0,0 +1,413 @@
+//interface to Pegasus-QCD
+//The interface is a translation to C++ of the following Pegasus QCD files:
+//initevol.f nparton.f initinp.f inplmom1.f
+  
+//The files are translated and modified on order to use LHAPDF as input for the PDF moments
+//in N-space, and to use a different contour in the complex plane for the support points of the
+//Mellin inversion
+  
+#include "pegasus.h"
+
+#include "pdfevol.h"
+#include "LHAPDF/LHAPDF.h"
+
+int pegasus::nff;
+int pegasus::ivfns;
+
+void pegasus::init()
+{
+  // From:
+  // ..File: initevol.f    
+  //                   
+  //
+  // ..The global initialization for the unpolarized parton evolution. 
+  //
+  // ..This routine initializes some constants, the array  NA  of complex
+  //    support points (depending on  IFAST),  the corresponding weights
+  //    WN  for the Gauss quadrature employed for the Mellin inversion, 
+  //    and the simple harmonic sums S_i, i=1,...,6 on the support points.
+  //
+  // ..The corresponding arrays of N-space splitting functions, evolution
+  //    matrix elements and flavour-threshold operator matrix elements are 
+  //    then initialized (depending on  IMODEV, NPORD, IVFNS, NFF, FR2) 
+  //    by the respective subroutines and stored in common blocks by them. 
+  //
+  // ..The default values of the initialization parameters speficied below
+  //    can by overwritten either by reading the file  usrinit.dat  (for 
+  //    IPAR = 1)  or by calling the subroutine  USRINIT  (for IPAR = 2).
+  //
+  // =====================================================================
+
+  const int NFMIN = 3;
+  const int NFMAX = 6;
+
+
+  //Some constants and the two-dimensional Kronecker symbol
+  double EMC = 0.57721566490153;
+
+  rzeta_.zeta_[0] = EMC;
+  rzeta_.zeta_[1] = 1.644934066848226;
+  rzeta_.zeta_[2] = 1.202056903159594;
+  rzeta_.zeta_[3] = 1.082323233711138;
+  rzeta_.zeta_[4] = 1.036927755143370;
+  rzeta_.zeta_[5] = 1.017343061984449;
+
+  kron2d_.d_[0][0] =  fcx(complex <double> (1., 0.));
+  kron2d_.d_[1][0] =  fcx(complex <double> (0., 0.));
+  kron2d_.d_[0][1] =  fcx(complex <double> (0., 0.));
+  kron2d_.d_[1][1] =  fcx(complex <double> (1., 0.));
+  
+  // QCD colour factors
+  colour_.ca_ = 3.;
+  colour_.cf_ = 4./3.;
+  colour_.tr_ = 0.5;
+
+  // Some default settings of the external initialization parameters
+  // (standard-speed iterated VFNS evolution at NLO for mu_f/mu_r = 1)
+
+  ivfns = 1; //VFN evolution (read from LHAPDF)
+  nff = 4;   //in FFN evolution, number of flavours  (read from LHAPDF)
+  evmod_.imodev_ = evolution_mode; //mode of evolution: there are three available schemes for solving the evolution equations at NNLO. mode = 1 reproduces evolution in x-space
+  order_.npord_  = LHAPDF::getOrderPDF(); //order of evolution (read from LHAPDF)
+  
+  double FR2 = 1.;//ratio of mur2/muf2
+  frrat_.logfr_ = log(FR2);
+
+  nnused_.nmax_ = mellinint::mdim;
+
+  //Location of the Mellin inversion contour in the complex N plane 
+  //Support points NA(K) for the gauss integration
+  for (int i = 0; i < mellinint::mdim; i++)
+    {
+      moms_.na_[i] = fcx(mellinint::Np[i]);
+
+      //The lowest simple harmonic sums on the support points
+      fcomplex N1 = fcx(mellinint::Np[i] + 1.);
+      int one = 1;
+      int two = 2;
+      int three = 3;
+      int four = 4;
+      int five = 5;
+      hsums_.s_[0][i] = fcx(cx(psi_(N1)) + EMC);
+      hsums_.s_[1][i] = fcx(rzeta_.zeta_[1] - cx(dpsi_(N1,one)));
+      hsums_.s_[2][i] = fcx(rzeta_.zeta_[2] + 0.5 * cx(dpsi_(N1,two)));
+      hsums_.s_[3][i] = fcx(rzeta_.zeta_[3] - 1./6. * cx(dpsi_(N1,three)));
+      hsums_.s_[4][i] = fcx(rzeta_.zeta_[4] + 1./24. * cx(dpsi_(N1,four)));
+      hsums_.s_[5][i] = fcx(rzeta_.zeta_[5] - 1./120. * cx(dpsi_(N1,five)));
+    }
+
+  //Some more (internal) evolution and initialization parameters 
+  aspar_.naord_  = order_.npord_;
+  aspar_.nastps_ = alphas_steps; //number of steps for the Runge-Kutta integration of alphas beyond LO
+
+  
+  if (evmod_.imodev_ == 1 || evmod_.imodev_ == 2)
+    itord_.nuord_ = 15;
+  else
+    itord_.nuord_ = order_.npord_;
+
+  if (ivfns == 0)
+    {
+      nfused_.nflow_  = nff;
+      nfused_.nfhigh_ = nff;
+    }
+  else
+    {
+      nfused_.nflow_  = NFMIN;
+      nfused_.nfhigh_ = NFMAX;
+    }
+
+  //The N-space splitting functions, evolution operators etc.
+  betafct_();
+  pns0mom_();
+  psg0mom_();
+  lsgmom_();
+
+  if (order_.npord_ > 0)
+    {
+      pns1mom_();
+      psg1mom_();
+      usg1mom_();
+    }
+  if (itord_.nuord_ > 1) usg1hmom_();
+
+  if (order_.npord_ > 1)
+    {
+      pns2mom_();
+      psg2mom_();
+      uns2mom_();
+      usg2mom_();
+    }
+  if (itord_.nuord_ > 2) usg2hmom_();
+
+  if (ivfns != 0)
+    {
+      ans2mom_();
+      asg2mom_();
+    }
+
+  // ..File nparton.f
+  //
+  //
+  // ..The subroutine  NPARTON  for one (in general complex) Mellin moment 
+  //    N  of the parton densities evolved to the scale  M2 = mu_f^2  by 
+  //    EVNFFN  (for IVFNS = 0)  or  EVNVFN.  
+  //
+  // ..The results are returned as  f_i(N,mu_f^2)  by the array  PDFN.
+  //    See the header of evnffn.f for the notation for f_i depending
+  //    on the input parameter  IPSTD.  For  IPOL = 0  we are dealing
+  //    with the unpolarized, otherwise with the polarized case.
+  //
+  // ..Note that, unlike its x-space counterpart  XPARTON  the present
+  //    routine includes calls of  INITPOL (.., EPAR)  and  INITINP (IPAR) 
+  //    or  INITPINP (IPAR),  i.e., for the N-space evolution this routine 
+  //    is the only one the user needs to call.  Input and initialization
+  //    parameters are specified as usual depending on  EPAR  and  IPAR.
+  // 
+  // =====================================================================
+
+  //Initialize the input parameters and initial moments
+  //
+  // ..File: initinp.f
+  //
+  //
+  // ..The input initialization for the unpolarized parton evolution.
+  //
+  // ..This routine initializes the parton distributions at the initial
+  //    scale  M20 (in GeV^2)  and, in the VFNS case, at the flavour
+  //    thresholds,  Mi2, i = C, B, T, together with the corresponding
+  //    values of the strong coupling constant. The corresponding routines
+  //    are  INPLMOMj  and  EVNFTHR  where more information can be found.
+  //    So far  INPLMOM1,2  are available and selected by  NFORM = 1, 2. 
+  //
+  // ..The flavour parameters  IVFNS, NFF  and the (fixed) scale log
+  //    LOGFR = ln(mu_f^2/mu_r^2)  are provided by the common blocks
+  //    VARFLV, NFFIX  and  FRRAT.  If called with  IPAR  unequal 1 or 2,
+  //    the initial conditions for the evolution are those of the 2001
+  //    Les Houches benchmark.  Other inputs (including the choice of
+  //    NFORM  are specified by reading the file  usrinp.f  (for IPAR = 1)
+  //    of by calling the routine  USRINP  (for IPAR = 2).  
+  //
+
+  //Some default settings of scales, couplings and input distributions
+
+  //Initial scale  M20 = M_0^2 (in GeV^2)  and  ASI = alpha_s(M_0^2)
+  //input values from LHAPDF
+  asinp_.m20_ = LHAPDF::getQ2min(opts.LHAPDFmember);
+  double ASI = LHAPDF::alphasPDF(sqrt(asinp_.m20_));
+
+  //The heavy quark masses squared, input values from LHAPDF
+  asfthr_.m2c_ = pow(LHAPDF::getThreshold(4),2);
+  asfthr_.m2b_ = pow(LHAPDF::getThreshold(5),2);
+  asfthr_.m2t_ = pow(LHAPDF::getThreshold(6),2);
+  
+  //Stop some nonsense
+  if (ivfns == 1 && asinp_.m20_ > asfthr_.m2c_)
+    {
+      cout << "Too high mu_0 for VFNS evolution. STOP" << endl;
+      exit (-1);
+    }
+
+  if (ASI > 2. || ASI < 2.0e-2 )
+    {
+      cout << "alpha_s out of range. STOP" << endl;
+      exit (-1);
+    }
+
+  if (ivfns == 1 && asfthr_.m2c_ > asfthr_.m2b_)
+    {
+      cout << "wrong charm-bottom mass hierarchy. stop" << endl;
+      exit (-1);
+    }
+  if (ivfns == 1 && asfthr_.m2b_ > asfthr_.m2t_)
+    {
+      cout << "Wrong bottom-top mass hierarchy. STOP" << endl;
+      exit (-1);
+    }
+
+  //For mu_r unequal mu_f  AS0  is different from the input parameter 
+  //ASM = a_s(M_0^2). The VFNS evolution starts with n_f = 3 at M_0^2.
+
+  double ASM = ASI / (4.* M_PI);
+  double R20 = asinp_.m20_ * exp(-frrat_.logfr_);
+  int NF = nff;
+  if (ivfns != 0)  NF  = 3;
+  asinp_.as0_ = as_(R20, asinp_.m20_, ASM, NF);
+
+  //  cout<< " alphas at the starting scale " << asinp_.as0_ << endl;
+  //  cout<< " alphas LHAPDF " << LHAPDF::alphasPDF(sqrt(asinp_.m20_))/ (4.* M_PI) << endl;
+  
+  //Input initialization (including threshold values for the VFNS case)
+  // ..File: inplmom1.f 
+  //
+  //
+  // ..The subroutine  INPLMOM1  (version 1) for the initial light-parton 
+  //    distributions of a non-strange hadron in N-space.  The moments are 
+  //    stored in the common-block  PAINP for an external NDIM-dimensional
+  //    array  NA  of complex moments specified in the common-block  MOMS.
+  //
+  // ..The functional form of the distributions for this version reads
+  //
+  //        xf = Nf * x^Pf(2) * (1 - x)^Pf(3)  
+  //             * ( 1 + x^Pf(4) * Pf(5) + x * Pf(6) )                 (1)
+  //
+  //    for  f = UV = u - ubar,  DV = d - dbar,  DL = dbar - ubar, 
+  //    LS = 2 * (dbar + ubar),  SM = s - sbar,  SS = s + sbar,   GL = g. 
+  //
+  // ..The normalization factors  NUV and NDV  are fixed by the respective 
+  //    quark numbers given by  PUV(1)  and  PDV(1)  (i.e.  PUV(1) = 2 and
+  //    PDV(1) = 1  for the proton).
+  //    NDL = PDL(1);  for  IMOMIN = 0  also  NLS = PLS(1), NSS = PSS(1).
+  //    For non-zero  IMOMIN,  PLS(1) and PSS(1)  represent the respective
+  //    momentum fractions.  NGL  is fixed by the momentum sum  PGL(1)  of
+  //    all partons.  For non-zero  ISSIMP,  SS = PSS(1) * LS  is used for 
+  //    the strange sea instead of (1), and  SM  is set to zero. Otherwise
+  //    the coefficient of x^PSM(4) is such that the first moment is zero.
+  //
+  // ..The normalization factors  Nf  and the momentum fractions  Af  are 
+  //    written to the common-block PANORM.  The flag  IINNEW  in  INPNEW
+  //    is set to '1' at the end of this routine.
+  //
+
+  //Begin of the Mellin-N loop 
+  fcomplex uval,dval,ubar,dbar,s,sbar,glu,charm,bot;
+  for (int i = 0; i < mellinint::mdim; i++)
+    {
+      int hadron = 1; //opts.ih1;
+      fcomplex N = fcx(mellinint::Np[i]); //compute positive branch only, the negative branch is obtained by complex conjugation
+      double facscale = sqrt(asinp_.m20_);
+      pdfmoments_(hadron,facscale,N,uval,dval,ubar,dbar,s,sbar,glu,charm,bot);
+//      cout << "pegasus init " << facscale << "  "
+//	   << i << "  " << mellinint::Np[i] << "  "
+//	   << cx(glu) << "  "
+//	   << cx(uval) << "  " << cx(dval) << "  "
+//	   << cx(ubar) << "  " << cx(dbar) << "  "
+//	   << cx(s) << "  "
+//	   << cx(charm) << "  " << cx(bot) << endl;
+      
+      painp_.gli_[i] = glu;
+      
+      //Arrays of non-singlet and singlet quark combinations for N_f = 3
+      painp_.vai_[i] = fcx(cx(uval) + cx(dval) + (cx(s)-cx(sbar)));
+      painp_.m3i_[i] = fcx(cx(uval) - cx(dval));
+      painp_.m8i_[i] = fcx(cx(painp_.vai_[i]) - 3.*(cx(s)-cx(sbar)));
+
+      painp_.sgi_[i] = fcx(cx(uval) + cx(dval) + 2.* (cx(dbar) + cx(ubar)) + (cx(s)+cx(sbar)));
+      painp_.p3i_[i] = fcx(cx(painp_.m3i_[i]) - 2.* (cx(dbar)-cx(ubar)));
+      painp_.p8i_[i] = fcx(cx(painp_.sgi_[i]) - 3.* (cx(s)+cx(sbar)));
+    }
+ 
+  if (ivfns != 0)
+    evnfthr_(asfthr_.m2c_, asfthr_.m2b_, asfthr_.m2t_);
+}
+
+void pegasus::evolve()
+{
+  //Values of a_s and N_f for the call of EVNFFN or EVNVFN below
+  double M2 = pow(fabs(pdfevol::bstarscale),2); //pdfevol::bscale
+
+  //check starting scale
+  //  double M2 = asinp_.m20_*10;
+  //  pdfevol::bstarscale = sqrt(M2);
+
+  double R2  = M2 * exp(-frrat_.logfr_);
+
+  //The values of alphas: ASF for the evolution are obtained
+  //using the Pegasus alphas function. In order to use the LHAPDF alphas function
+  //the code which calculates the evolution at the thresholds should be changed
+
+  double ASI, ASF;
+  int NF;
+
+  if (ivfns == 0)  //Fixed number of flavours
+    {
+      NF  = nff;
+      double R20 = asinp_.m20_ * R2/M2;
+      ASI = asinp_.as0_;
+      ASF = as_(R2, R20, asinp_.as0_, NF);
+    }
+  else      //Variable number of flavours
+    {
+      if (M2 > asfthr_.m2t_)
+	{
+	  NF = 6;
+	  double R2T = asfthr_.m2t_ * R2/M2;
+	  ASI = asfthr_.ast_;
+	  //ASF = LHAPDF::alphasPDF(sqrt(M2)) / (4.* M_PI);
+	  ASF = as_(R2, R2T, asfthr_.ast_, NF);
+	}
+      else if (M2 > asfthr_.m2b_)
+	{
+	  NF = 5;
+	  double R2B = asfthr_.m2b_ * R2/M2;
+	  ASI = asfthr_.asb_;
+	  //ASF =  LHAPDF::alphasPDF(sqrt(M2)) / (4.* M_PI);
+	  ASF =  as_(R2, R2B, asfthr_.asb_, NF);
+	}
+      else if (M2 > asfthr_.m2c_)
+	{
+	  NF = 4;
+	  double R2C = asfthr_.m2c_ * R2/M2;
+	  ASI = asfthr_.asc_;
+	  //ASF =  LHAPDF::alphasPDF(sqrt(M2)) / (4.* M_PI);
+	  ASF =  as_(R2, R2C, asfthr_.asc_, NF);
+	}
+      else
+	{
+	  NF = 3;
+	  double R20 = asinp_.m20_ * R2/M2;
+	  ASI = asinp_.as0_;
+	  //ASF =  LHAPDF::alphasPDF(sqrt(M2)) / (4.* M_PI);
+	  ASF =  as_(R2, R20, asinp_.as0_, NF);
+	}
+    }
+
+  //Calculation of the moments of the parton densities and output
+  int nlow = 1;
+  int nhigh = mellinint::mdim;
+  int IPSTD = 1;
+  fcomplex PDFN[13][144];
+  if (ivfns == 0)
+    evnffn_(PDFN, ASI, ASF, NF, nlow, nhigh, IPSTD);
+  else
+    evnvfn_(PDFN, ASI, ASF, NF, nlow, nhigh, IPSTD);
+
+
+  for (int i = 0; i < mellinint::mdim; i++)
+    {
+      complex <double> fx[11];
+      for (int p = -MAXNF; p <= MAXNF; p++)
+	fx[p+MAXNF] = cx(PDFN[p+6][i]);
+      pdfevol::storemoments(i, fx);
+    }
+  
+
+  //Compare Pegasus QCD evolution with direct Mellin transform
+  /*
+  for (int i = 0; i < mellinint::mdim; i++)
+    {
+      pdfevol::calculate (i);
+      cout << "direct " << fabs(pdfevol::bstarscale) << "  "
+  	   << i << "  " << mellinint::Np[i] << "  "
+  	   << cx(creno_.cfx1_[i][0+MAXNF]) << "  "
+  	   << cx(creno_.cfx1_[i][1+MAXNF])-cx(creno_.cfx1_[i][-1+MAXNF]) << "  "
+  	   << cx(creno_.cfx1_[i][2+MAXNF])-cx(creno_.cfx1_[i][-2+MAXNF]) << "  "
+  	   << cx(creno_.cfx1_[i][-1+MAXNF]) << "  "
+  	   << cx(creno_.cfx1_[i][-2+MAXNF]) << "  "
+  	   << cx(creno_.cfx1_[i][3+MAXNF]) << "  "
+	   << cx(creno_.cfx1_[i][4+MAXNF]) << "  "
+	   << cx(creno_.cfx1_[i][5+MAXNF]) << endl;
+  
+      cout << "pegasus " << sqrt(M2) << "  "
+  	   << i << "  " << mellinint::Np[i] << "  "
+  	   << cx(PDFN[0+6][i]) << "  "
+  	   << cx(PDFN[1+6][i])-cx(PDFN[-1+6][i]) << "  "
+  	   << cx(PDFN[2+6][i])-cx(PDFN[-2+6][i]) << "  "
+  	   << cx(PDFN[-1+6][i]) << "  "
+  	   << cx(PDFN[-2+6][i]) << "  "
+  	   << cx(PDFN[3+6][i]) << "  "
+       	   << cx(PDFN[4+6][i]) << "  "
+       	   << cx(PDFN[5+6][i]) << endl;
+    }
+  */
+}
---
+++ resum/pegasus.h
@@ -0,0 +1,131 @@
+#ifndef pegasus_h
+#define pegasus_h
+
+#include "interface.h"
+
+namespace pegasus
+{
+  //settings of the interface
+  const int evolution_mode = 1; //mode of evolution: there are three available schemes for solving the evolution equations at NNLO. mode = 1 reproduces evolution in x-space
+  const int alphas_steps = 20;  //number of steps for the Runge-Kutta integration of alphas beyond LO
+ 
+  extern void init();
+  extern void evolve();
+
+  extern int nff;
+  extern int ivfns;
+}
+
+
+//fortran interface to pegasus common blocks and functions
+const int ndim = 144; //maximum dimension of the moments array
+
+extern "C" {
+
+  extern struct {
+    int imodev_;
+  } evmod_;
+
+  extern struct {
+    int npord_;
+  } order_;
+
+  extern struct {
+    double logfr_;
+  } frrat_;
+
+  extern struct {
+    int naord_;
+    int nastps_;
+  } aspar_;
+
+  extern struct {
+    int nflow_;
+    int nfhigh_;
+  } nfused_;
+
+  extern struct {
+    int nuord_;
+  } itord_;
+  
+  extern struct {
+    int nmax_;
+  } nnused_;
+
+  extern struct {
+    fcomplex na_[ndim];
+  } moms_;
+
+  extern struct {
+    fcomplex s_[6][ndim];
+  } hsums_;
+  
+  extern struct {
+    double zeta_[6];
+  } rzeta_;
+       
+  extern struct {
+    fcomplex d_[2][2];
+  } kron2d_;
+
+  extern struct {
+    double cf_;
+    double ca_;
+    double tr_;
+  } colour_;
+
+
+  extern struct {
+    double as0_;
+    double m20_;
+  } asinp_;
+
+  extern struct {
+    double asc_;
+    double m2c_;
+    double asb_;
+    double m2b_;
+    double ast_;
+    double m2t_;
+  } asfthr_;
+
+  fcomplex psi_ (fcomplex &z);
+  fcomplex dpsi_ (fcomplex &z, int &m);
+
+  extern void betafct_();
+  extern void pns0mom_();
+  extern void psg0mom_();
+  extern void lsgmom_();
+  extern void pns1mom_();
+  extern void psg1mom_();
+  extern void usg1mom_();
+  extern void usg1hmom_();
+  extern void pns2mom_();
+  extern void psg2mom_();
+  extern void uns2mom_();
+  extern void usg2mom_();
+  extern void usg2hmom_();
+  extern void ans2mom_();
+  extern void asg2mom_();
+
+  extern double as_ (double &R2, double &R20, double &AS0, int &NF);
+  
+  extern void evnfthr_(double &MC2, double &MB2, double &MT2);
+  
+  extern void evnvfn_(fcomplex PDFN[13][ndim], double &ASI, double &ASF, int &NF, int &NLOW, int &NHIGH, int &IPSTD);
+  extern void evnffn_(fcomplex PDFN[13][ndim],  double &ASI,  double &ASF, int &NF, int &NLOW, int &NHIGH, int &IPSTD);
+
+  extern struct {
+    fcomplex vai_[ndim];
+    fcomplex m3i_[ndim];
+    fcomplex m8i_[ndim];
+    fcomplex sgi_[ndim];
+    fcomplex p3i_[ndim];
+    fcomplex p8i_[ndim];
+    fcomplex gli_[ndim];
+  } painp_;
+  
+}
+
+
+#endif
