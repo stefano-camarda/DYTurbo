1bdfb29ece994cb8af39ef39f91a7b2dbc5a7958
Stefano Camarda <stefano.camarda@cern.ch>
2016-04-19 23:34:01 +0200

    banner files

---
+++ src/banner.C
@@ -0,0 +1,21 @@
+#include "banner.h"
+#include "config.h"
+
+#include <iostream>
+
+using namespace std;
+void banner()
+{
+  cout << endl << endl;
+  cout << " (        )              (           )   " << endl;
+  cout << " )\\ )  ( /(   *   )      )\\ )  (  ( /(   " << endl;
+  cout << "(()/(  )\\())` )  /(   ( (()/(( )\\ )\\())  " << endl;
+  cout << " /(_))((_)\\  ( )(_))  )\\ /(_))((_|(_)\\   " << endl;
+  cout << "(_))___ ((_)(_(_())_ ((_|_))((_)_  ((_)  " << endl;
+  cout << " |   \\ \\ / /|_   _| | | | _ \\| _ )/ _ \\  " << endl;
+  cout << " | |) \\ V /   | | | |_| |   /| _ \\ (_) | " << endl;
+  cout << " |___/ |_|    |_|  \\___/|_|_\\|___/\\___/  " << endl;
+  cout << "                                   v " << VERSION << endl;
+  cout << endl;
+  cout << endl;
+}
