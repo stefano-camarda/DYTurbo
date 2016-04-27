1261f7e17e1f5c6d28fb4002a8aa445c9567446a
Jakub <jakub.cuth@cern.ch>
2016-04-27 08:36:53 +0200

    Run grid script. Inspired by Fabrice.

--- scripts/run_grid.sh
+++ scripts/run_grid.sh
@@ -8,15 +8,23 @@
 ## @author cuto <Jakub.Cuth@cern.ch>
 ## @date 2015-10-27
 
+jobname=$1
+ln -sf infiles/${jobname}.in input.in
+random_seed=$2
+sed -i "s|= seed|= 1$random_seed|g        " input.in
+
+
 # setup ENV
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 # LHAPDF and ROOT
-lsetup "root --skipConfirm"
-#lsetup "sft MCGenerators_lcgcmt67c/lhapdf/6.1.5 " 
+lsetup "root 6.04.14-x86_64-slc6-gcc49-opt"
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/lhapdf/6.1.5-6570e/x86_64-slc6-gcc49-opt/bin:$PATH
+lhapdf-config --version || exit 2
 # own lhapdf set
-#export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/
-export LHAPDF_DATA_PATH=./
+#export LHAPDF_DATA_PATH=./
+# official sets
+export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/
 export LHAPATH=$LHAPDF_DATA_PATH
 # dyturbo libs
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./lib
@@ -29,11 +37,6 @@ echo
 # compile just for sure
 #make install
 
-jobname=$1
-ln -sf scripts/infiles/$jobname.in input.in
-random_seed=$2
-sed -i "s|= seed|= 1$random_seed|g        " input.in
-
 echo
 echo input.in :
 cat input.in
@@ -42,3 +45,5 @@ rm -f results*.root
 
 /usr/bin/time -v ./bin/dyturbo input.in
 hadd -f results_merge.root results*.root
+
+
