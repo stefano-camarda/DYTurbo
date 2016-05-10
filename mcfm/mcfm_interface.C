509da02038f5457798ae6c9065a2e8ac1eb79e33
Stefano Camarda <stefano.camarda@cern.ch>
2016-05-10 10:30:59 +0200

    moved functions and common blocks from interface to mcfm_interface

---
+++ mcfm/mcfm_interface.C
@@ -0,0 +1,191 @@
+#include "mcfm_interface.h"
+#include "settings.h"
+#include "coupling.h"
+#include "resconst.h"
+
+#include <cstring>
+#include <iostream>
+
+map <int,string> plabel;
+
+void mcfm::init()
+{
+  energy_.sroot_              = opts.sroot;
+  density_.ih1_               = opts.ih1;
+  density_.ih2_               = opts.ih2;
+  nproc_.nproc_               = opts.nproc;
+  zerowidth_.zerowidth_       = opts.zerowidth;
+  dynamicscale_.dynamicscale_ = opts.dynamicscale;   // Dynamic scale (if true mufac=muren=q)
+
+  //    CKM matrix entries
+  cabib_.Vud_ = opts.Vud;
+  cabib_.Vus_ = opts.Vus;
+  cabib_.Vub_ = opts.Vub;
+  cabib_.Vcd_ = opts.Vcd;
+  cabib_.Vcs_ = opts.Vcs;
+  cabib_.Vcb_ = opts.Vcb;
+
+  dymasses_.wwidth_ = opts.wwidth;
+  dymasses_.zwidth_ = opts.zwidth;
+  
+  //initialise MCFM settings
+  flag_.flag_ = false;
+
+  noglue_.noglue_=false;
+  noglue_.ggonly_=false;
+  noglue_.gqonly_=false;
+
+  string part = "tota";
+  strncpy(part_.part_ , part.c_str(), part.size());
+
+  colc_.colourchoice_=0;
+  double rtsmin = 40;
+  cutoff_.cutoff_=0.001; //add to settings (this is a cut off in mcfm on invariant mass pairs between emitted and radiator)
+
+  flags_.qflag_=true;
+  flags_.gflag_=true;
+
+  //Catani-Seymour subtraction cut-offs for initial-initial, initial-final, final-initial, and final-final dipoles
+  //(add to settings)
+  alfacut_.aii_=1.;//0.01;
+  alfacut_.aif_=1.;//0.01;
+  alfacut_.afi_=1.;//0.01;
+  alfacut_.aff_=1.;//0.01;
+
+  //Limits on invariant mass of vector boson decay products
+  //(irrelevant if zerowidth=true)
+  limits_.wsqmin_=pow(opts.mlow,2);
+  limits_.wsqmax_=pow(opts.mhigh,2);
+
+  //Check if the limits are compatible with sroot
+  if (limits_.wsqmax_> pow(energy_.sroot_,2))
+    limits_.wsqmax_=pow(energy_.sroot_,2);
+
+  plabel[1]="pp";
+  plabel[2]="pp";
+
+  //dycoupling_();
+  coupling::init();
+
+  dymasses_.mb_=0; //probably irrelevant to set the bottom mass to 0, since it is not used anywhere
+     
+  if (!((density_.ih1_==1) && (density_.ih2_ == -1))
+      && !((density_.ih1_==1) && (density_.ih2_ == 1)))
+    {
+      cout << "The selected initial state is not valid," << endl;
+      cout << "please select proton-proton (1 1) or proton-antiproton (1 -1)" << endl;
+      exit(0);
+    }
+
+  //the default behaviour is to remove no branching ratio
+  bool removebr=false;
+  brnrat_.brnrat_=1.;
+
+  //branching ratios
+  double brwen,brzee,brtau,brtop;
+  branch_(brwen,brzee,brtau,brtop);
+
+  //W+->l+nubar
+  if (nproc_.nproc_ == 1)
+    {
+      plabel[3]="nl";
+      plabel[4]="ea";
+      plabel[5]="pp";
+      plabel[6]="pp";
+      nwz_.nwz_=1;
+      breit_.mass3_= dymasses_.wmass_;
+      breit_.width3_= dymasses_.wwidth_;
+      opts.rmass = dymasses_.wmass_;
+      opts.rwidth = dymasses_.wwidth_;
+      if (removebr)
+        brnrat_.brnrat_=brwen;
+      
+    }
+  //W-=>l-nu
+  else if(nproc_.nproc_ == 2)
+    {
+      plabel[3]="el";
+      plabel[4]="na";
+      plabel[5]="pp";
+      plabel[6]="pp";
+      nwz_.nwz_=-1;
+      breit_.mass3_=dymasses_.wmass_;
+      breit_.width3_=dymasses_.wwidth_;
+      opts.rmass = dymasses_.wmass_;
+      opts.rwidth = dymasses_.wwidth_;
+      if (removebr)
+        brnrat_.brnrat_=brwen;
+    }
+  else if(nproc_.nproc_==3)
+    {
+      plabel[3]="el";
+      plabel[4]="ea";
+      plabel[5]="pp";
+      plabel[6]="pp";
+      nwz_.nwz_=0;
+      breit_.mass3_=dymasses_.zmass_;
+      breit_.width3_=dymasses_.zwidth_;
+      opts.rmass = dymasses_.zmass_;
+      opts.rwidth = dymasses_.zwidth_;
+
+      zcouple_.l1_=zcouple_.le_;
+      zcouple_.r1_=zcouple_.re_;
+
+      //with q1=0 the photon contribution is switched off
+      //with q1=-1 the photon contribution is switched on
+
+      dyphoton_.phot_=zcouple_.q1_;
+
+      if (removebr)
+	brnrat_.brnrat_=brzee;
+    }
+  else
+    {
+      cout << "Wrong process" << endl;
+      exit(0);
+    }
+
+  //Breit Wigner unweighting settings
+  //the jets, particles 5 and 6, are not resonant
+  breit_.n2_=0; 
+  breit_.mass2_=0;
+  breit_.width2_=0;
+
+  //the leptons, particles 3 and 4, are resonant
+  breit_.n3_=1;
+
+  //Decide if worth using Breit-Wigner unweighting or not for the dilepon system
+  //n3=0
+  //vmass=wmass
+  //if(nproc.eq.3) vmass=zmass
+  //if(mwmin.lt.vmass.and.mwmax.gt.vmass) n3=1
+
+  ckmfill_(nwz_.nwz_);
+
+  // Set-up PS integration cut-offs
+  rtsmin = min (rtsmin, sqrt(limits_.wsqmin_ + cutoff_.cutoff_));
+
+  if (zerowidth_.zerowidth_)
+    {
+      rtsmin = dymasses_.wmass_;
+      if (nproc_.nproc_ == 3) rtsmin = dymasses_.zmass_;
+    }
+
+  taumin_.taumin_=pow((rtsmin/energy_.sroot_),2);
+  taumin_.logtaumin_=log(taumin_.taumin_);
+  xmin_.xmin_=taumin_.taumin_;
+
+  //Set-up incoming beams (used only in realint.f)
+  pext_.p1ext_[3]=-0.5*energy_.sroot_;
+  pext_.p1ext_[0]=0.;
+  pext_.p1ext_[1]=0.;
+  pext_.p1ext_[2]=-0.5*energy_.sroot_;
+
+  pext_.p2ext_[3]=-0.5*energy_.sroot_;
+  pext_.p2ext_[0]=0.;
+  pext_.p2ext_[1]=0.;
+  pext_.p2ext_[2]=+0.5*energy_.sroot_;
+
+  //set NF to 5 (it is used in H2calc)
+  nf_.nf_ = resconst::NF;
+}
---
+++ mcfm/mcfm_interface.h
@@ -0,0 +1,297 @@
+#ifndef mcfm_interface_h
+#define mcfm_interface_h
+
+#define MAXNF 5
+
+extern "C"
+{
+  //interface to MCFM fortran functions and common blocks
+  
+  void breitw_(double& x1, double& mminsq, double& mmaxsq, double& rmass, double& rwidth, double& msq, double& wt);
+  void boost_(double& mass, double p1[],double p_in[], double p_out[]);
+  void branch_(double& brwen, double& brzee, double& brtau, double& brtop);
+  void ckmfill_(int& nwz);
+  void scaleset_(double& q2);
+
+  //Catani-Seymour subtraction cut-offs for initial-initial, initial-final, final-initial, and final-final dipoles
+  extern struct {
+    double aii_;
+    double aif_;
+    double afi_;
+    double aff_;
+  } alfacut_;
+
+  //initialization flag
+  extern struct {
+    int flag_;
+  } flag_;
+
+  extern struct {
+    int qflag_;
+    int gflag_;
+  } flags_;
+
+  extern struct {
+    int noglue_;
+    int ggonly_;
+    int gqonly_;
+  } noglue_;
+
+  extern struct {
+    int colourchoice_;
+  } colc_;
+
+
+  // z coupling
+  extern struct {
+    double l_[MAXNF];
+    double r_[MAXNF];
+    double q1_;
+    double l1_;
+    double r1_;
+    double q2_;
+    double l2_;
+    double r2_;
+    double le_;
+    double ln_;
+    double re_;
+    double rn_;
+    double sin2w_;
+  } zcouple_;
+
+  extern struct {
+    int phot_;
+  } dyphoton_;
+
+  // masses
+  extern struct {
+    double md_;
+    double mu_;
+    double ms_;
+    double mc_;
+    double mb_;
+    double mt_;
+    double mel_;
+    double mmu_;
+    double mtau_;
+    double hmass_;
+    double hwidth_;
+    double wmass_;
+    double wwidth_;
+    double zmass_;
+    double zwidth_;
+    double twidth_;
+    double tauwidth_;
+    double mtausq_;
+    double mcsq_;
+    double mbsq_;
+  } dymasses_;
+
+  // ewinput
+  extern struct {
+    double Gf_inp_;
+    double aemmz_inp_;
+    double xw_inp_;
+    double wmass_inp_;
+    double zmass_inp_;
+  } ewinput_;
+
+  //EW scheme
+  extern struct {
+    int ewscheme_;
+  } ewscheme_;
+
+  // ewcouple
+  extern struct {
+    double Gf_;
+    double gw_;
+    double xw_;
+    double gwsq_;
+    double esq_;
+    double vevsq_;
+  } ewcouple_;
+
+  //alpha EM (MZ)
+  /*  extern struct {
+      double aemmz_;
+      } em_;*/
+
+  extern struct {
+    int nf_;
+  } nf_;
+
+  extern struct {
+    double Q_[2*MAXNF+1];
+    double tau_[2*MAXNF+1];
+  } ewcharge_;
+
+  //branching ratio
+  extern struct {
+    double brnrat_;
+  } brnrat_;
+
+  //boson charge (used for the CKM matrix)
+  extern struct {
+    int nwz_;
+  } nwz_;
+
+  extern struct {
+    int n2_;
+    int n3_;
+    double mass2_;
+    double width2_;
+    double mass3_;
+    double width3_;
+  } breit_;
+
+  // CKM
+  extern struct {
+    double Vud_;
+    double Vus_;
+    double Vub_;
+    double Vcd_;
+    double Vcs_;
+    double Vcb_;
+  } cabib_;
+
+  //QCD coupling
+  extern struct {
+    double gsq_;
+    double as_;
+    double ason2pi_;
+    double ason4pi_;
+  } qcdcouple_;
+
+  extern struct {
+    double b0_;
+  } b0_;
+
+  /*  // H+b mb msbar value
+      extern struct {
+      double mb_msbar_;
+      } mb_msbar_;*/
+
+  // dimensional regularization parameters
+  extern struct {
+    double epinv_;
+  } epinv_;
+
+  extern struct {
+    double epinv2_;
+  } epinv2_;
+
+  //input file related variables
+  extern struct {
+    double sroot_;
+  } energy_;
+
+  extern struct {
+    int ih1_;
+    int ih2_;
+  } density_;
+
+  extern struct {
+    int nproc_;
+  }  nproc_;
+
+  extern struct {
+    int dynamicscale_;
+  } dynamicscale_;
+
+  extern struct {
+    double scale_;
+    double musq_;
+  } scale_;
+
+  extern struct {
+    double facscale_;
+  } facscale_;
+
+  //dynamic scale for each dipole
+  extern struct {
+    double dipscale_[41];
+  } dipolescale_;
+
+  extern struct {
+    char part_[4];
+  } part_;
+
+  extern struct {
+    int zerowidth_;
+  } zerowidth_;
+
+  /*  extern struct {
+      double Mwmin_;
+      double Mwmax_;
+      } mwminmax_;*/
+
+  /*extern struct {
+    int itmx1_;
+    int ncall1_;
+    int itmx2_;
+    int ncall2_;
+    } iterat_;*/
+
+  /*  extern struct {
+      int rseed_;
+      } rseed_;*/
+
+  /*  extern struct {
+      int iset_;
+      } pdfiset_;*/
+
+  /*extern struct {
+    double nset_;
+    char prefix_[50];
+    } prefix_;*/
+
+  /*  extern struct {
+      char PDFname_[30];
+      } lhapdf_char_;
+
+      extern struct {
+      int PDFmember_;
+      } lhapdf_int_;*/
+
+  /*extern struct {
+    char runstring_[30];
+    } runstring_;*/
+
+  /*  extern struct {
+      int pr_;
+      } pr_;*/
+
+  /*  extern struct {
+      double rtsmin_;
+      } rtsmin_;*/
+
+  extern struct {
+    double wsqmin_;
+    double wsqmax_;
+  } limits_;
+
+  extern struct {
+    double taumin_;
+    double logtaumin_;
+  } taumin_;
+
+  extern struct {
+    double xmin_;
+  } xmin_;
+
+  extern struct {
+    double p1ext_[4];
+    double p2ext_[4];
+  } pext_;
+
+  //mcfm cut off on invariant mass pairs between emitted and radiator
+  extern struct {
+    double cutoff_;
+  } cutoff_;
+}  
+
+namespace mcfm
+{
+  void init();
+}
+#endif
