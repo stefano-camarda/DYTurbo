732bed11c241f6ea36d9451dad662fc55fb96e06
Jakub <jakub.cuth@cern.ch>
2016-06-02 09:00:46 +0200

    LXBATCH script fix:
    - general lhapdf path
    - profilled pdf

--- scripts/run_chain.sh
+++ scripts/run_chain.sh
@@ -74,6 +74,22 @@
 #./scripts/submit_DYTURBO.sh --mogon --proc wp,wm --term RES2P,CT2P --pdfset CT10nnlo --pdfvar array
 #./scripts/merge.sh --proc wp,wm --term RES2P,CT2P --qtymerge qt010y-11 --outdir CT10nnlo_RESCT2P_160512
 
+
+##  -- PROFILED PDFS
+
+## PROFILLED MMHT
+#./scripts/submit_DYTURBO.sh --lxbatch --proc z0 --term RES2D,CT2D --pdfset MMHTProf68cl --pdfvar array RUN
+#./scripts/merge.sh --proc z0 --term RES2D,CT2D --pdfset MMHTProf68cl --qtymerge qt05y-11 --outdir MMHT14Prof_RESCT2D_160523
+
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm --term RES2D,CT2D --pdfset MMHTProf68cl --pdfvar array RUN
+#./scripts/merge.sh --proc wp,wm --term RES2D,CT2D --pdfset MMHTProf68cl --qtymerge qt05y-11 --outdir MMHT14Prof_RESCT2D_160523
+
+# rerun
+./scripts/submit_DYTURBO.sh --lxbatch --proc z0 --term CT2D  --pdfset MMHTProf68cl --pdfvar array RUN
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp --term RES2D --pdfset MMHTProf68cl --pdfvar array RUN
+
+#./scripts/merge.sh --proc wp,wm,z0 --term RES2D,CT2D --pdfset MMHTProf68cl --qtymerge qt05y-11 --outdir MMHT14Prof_RESCT2D_160523 #RUN
+
 #============================================
 # MERGE GRID DONE
 #============================================
@@ -86,7 +102,7 @@
 # ./scripts/merge.sh --proc wp,wm,z0 --term REAL,VIRT --indir  results_grid/group.perf-jets  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 RUN
 
 # MMHT
-#./scripts/merge.sh --proc wp,wm,z0 --term REAL,VIRT --indir  results_grid/group.perf-jets --pdfset MMHT2014nnlo68cl  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 RUN
+#./scripts/merge.sh --proc wp,wm,z0 --term REAL,VIRT --indir  results_grid/group.perf-jets --pdfset MMHT2014nnlo68cl  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 #RUN
 
 #./scripts/merge.sh --proc wp --term VIRT --indir  results_grid/group.perf-jets --pdfset MMHT2014nnlo68cl  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 RUN
 #./scripts/merge.sh --proc wm --term REAL --indir  results_grid/group.perf-jets --pdfset MMHT2014nnlo68cl  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 RUN
@@ -96,14 +112,25 @@
 #CT14
 #./scripts/merge.sh --proc wp,wm,z0 --term REAL,VIRT --indir  results_grid/group.perf-jets --pdfset CT14nnlo  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524
 
+# download profilled
+#rucio ls group.perf-jets.dyturbo_*_lhc7_*_all_o2qt050y-55t*_seed_v1463677142_00_results_merge.root
+
+#rucio ls group.perf-jets.dyturbo_*_lhc7_MMHTProf68cl_all_o2qt050y-55t*_seed_v1463677142_00_results_merge.root
+#./scripts/merge.sh --proc wp,wm,z0 --term REAL,VIRT --indir  results_grid/group.perf-jets --pdfset MMHTProf68cl  --gridmerge v146.*_results_merge --outdir grid_fabrice_160524 RUN
 
 
 
+#============================================
 # FIXED ORDER
+#============================================
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset CT10nnlo --pdfvar array --term FIXCT2D RUN
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset CT14nnlo --pdfvar array --term FIXCT2D RUN
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset MMHT2014nnlo68cl --pdfvar array --term FIXCT2D RUN
 
-./scripts/submit_DYTURBO.sh --mogon --proc wp,wm,z0 --pdfset CT10nnlo --pdfvar array --term FIXCT2D
-./scripts/submit_DYTURBO.sh --mogon --proc wp,wm,z0 --pdfset CT14nnlo --pdfvar array --term FIXCT2D
-./scripts/submit_DYTURBO.sh --mogon --proc wp,wm,z0 --pdfset MMHT2014nnlo68cl --pdfvar array --term FIXCT2D
+# PROFILLED
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset CT10nnlo68clProfiled --pdfvar array --term FIXCT2D RUN
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset CT14nnloProf68cl --pdfvar array --term FIXCT2D RUN
+#./scripts/submit_DYTURBO.sh --lxbatch --proc wp,wm,z0 --pdfset MMHTProf68cl --pdfvar array --term FIXCT2D RUN
 
 #SKIP./scripts/submit_DYTURBO.sh --grid --proc wp,wm,z0 --pdfset CT10nnlo --pdfvar all --term VV --seeds 500
 # SKIP ./scripts/submit_DYTURBO.sh --mogon --proc wp,wm,z0 --pdfset CT10nnlo --pdfvar array --term FIXCT2D  RUN
