a8433e853a5c90fa993309fe153931fc4d9ca1b9
Jakub <jakub.cuth@cern.ch>
2016-06-17 10:12:20 +0200

    bug fix RNDM:0

--- scripts/run_prun.sh
+++ scripts/run_prun.sh
@@ -23,7 +23,7 @@ PRUN(){
         echo NOT TESTED $target && exit 6
  prun \
  --bexec "chmod 777 compile_grid.sh; ./compile_grid.sh  $DYTURBOVERSION;" \
- --exec "chmod 777 run_grid.sh; ./run_grid.sh ${arguments} --seed %RNDM ;" \
+ --exec "chmod 777 run_grid.sh; ./run_grid.sh ${arguments} --seed %RNDM:0 ;" \
  --extFile dyturbo-${DYTURBOVERSION}.tar.gz \
  --nJobs $njobs \
  --outDS ${CERNGROUP}.${CERNUSER}.${jobname}_${gridv}/ \
@@ -35,7 +35,7 @@ PRUN(){
     else
 
 prun \
---exec "chmod 777 run_grid.sh; ./run_grid.sh ${arguments} --seed %RNDM ;" \
+--exec "chmod 777 run_grid.sh; ./run_grid.sh ${arguments} --seed %RNDM:0 ;" \
 --nJobs $njobs \
 --maxFileSize=25000000 \
 --outDS ${CERNGROUP}.${CERNUSER}.${jobname}_${gridv}/ \
