0aa2199643f60bb9a888e11705c95fbe8976cead
Stefano Camarda <stefano.camarda@cern.ch>
2016-07-27 01:38:37 +0200

    LO order

--- src/init.C
+++ src/init.C
@@ -38,7 +38,7 @@ void dyturboinit(int argc, char * argv[])
   
   //Initialise some DYRES settings
   g_param_.g_param_ = opts.g_param;
-  nnlo_.order_ = opts.order;            //order (1=NLO, 2=NNLO)
+  nnlo_.order_ = opts.order;            //order (0=LO, 1=NLO, 2=NNLO)
   opts_.fixedorder_  = opts.fixedorder; //fixed order/resummation switch
   qtsub_.xqtcut_= opts.xqtcut;          //Cut on qt/Q
   qtsub_.qtcut_= opts.qtcut;            //Cut on qt
