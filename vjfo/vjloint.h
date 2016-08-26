646f581faae24b799109a65e8a5dc6915780d1b1
Stefano Camarda <stefano.camarda@cern.ch>
2016-08-26 17:46:34 +0200

    clean up

--- vjfo/vjloint.h
+++ vjfo/vjloint.h
@@ -6,7 +6,6 @@ namespace vjloint
   extern void init();
   extern double calc(const double x[5]);
   extern void fillp(double p[4][12]);
-  extern int x2int(unsigned ndim, const double x[], void *data, unsigned ncomp, double f[]);
 
   extern double muf, mur;
 }
