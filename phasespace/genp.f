12ba52beeea7d646bd3f666eb4d0cd08e0c6877b
Stefano Camarda <stefano.camarda@cern.ch>
2016-08-26 17:36:53 +0200

    fix in phi lep

--- phasespace/genp.f
+++ phasespace/genp.f
@@ -1,5 +1,6 @@
       subroutine genp(costh,phi,m,p)
 c     generate 4-momentum from costh, phi, m
+c     Important: phi must be in [-pi,pi]
       implicit none
       double precision costh, phi, m
       double precision p(4)
@@ -7,7 +8,7 @@ c     generate 4-momentum from costh, phi, m
       sintheta = sqrt(max(0d0,1d0-costh**2))
       cosphi = cos(phi);
       sinphi = sqrt(max(0d0,1d0-cosphi**2))
-      if (phi.lt.0) phi = -phi
+      if (phi.lt.0) sinphi = -sinphi
       p(4)=m/2d0                !E
       p(1)=p(4)*sintheta*sinphi !px
       p(2)=p(4)*sintheta*cosphi !py
