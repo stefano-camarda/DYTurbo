a231eb5bc657878a669624ac9eab5c77e5eca283
Stefano Camarda <stefano.camarda@cern.ch>
2016-08-31 23:21:31 +0200

    junk code

--- phasespace/mweight.f
+++ phasespace/mweight.f
@@ -25,3 +25,36 @@ c---- bw=(1d0+tanal**2)*rmass**2*rwidth**2
       jac=jac*(mmax-mmin)
       return
       end
+
+c  //split the mass in 3 pieces,
+c  //and unweight the breit wigner only from -5 width to +5 width
+c  //no sense, I have to split the mass before entering the integration
+c  //and set breit wigner unweighting or not
+c  double bwmn = opts.rmass -5*opts.rwidth;
+c  double bwmx = opts.rmass +5*opts.rwidth;
+c  double bwmnsq = pow(bwmn,2);
+c  double bwmxsq = pow(bwmx,2);
+c  double m,m2,wt;
+c  double xl = 0.25;
+c  double xu = 0.75;
+c  if (x[0] < xl)
+c    {
+c      double x1=x[0]/(xl-0.);
+c      m=phasespace::mmin+(bwmn-phasespace::mmin)*x[0];
+c      jac=jac*(bwmn-phasespace::mmin)/(xl-0.);
+c    }
+c  else if (x[0] > xu)
+c    {
+c      double x1=(x[0]-xu)/(1.-xu);
+c      m=bwmx+(phasespace::mmax-bwmx)*x1;
+c      jac=jac*(phasespace::mmax-bwmx)/(1.-xu);
+c    }
+c  else
+c    {
+c      double xbw=(x[0]-xl)/(xu-xl);
+c      breitw_(xbw,bwmnsq,bwmxsq,opts.rmass,opts.rwidth,m2,wt);
+c      m=sqrt(m2);
+c      wt=wt/(xu-xl);
+c      jac=jac*wt;
+c    }
+      
