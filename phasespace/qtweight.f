dc41ecbc9dd09cfc9163128e2c1bdbf641e6c586
Stefano Camarda <stefano.camarda@cern.ch>
2016-08-31 23:22:01 +0200

    res qt unweighting

--- phasespace/qtweight.f
+++ phasespace/qtweight.f
@@ -3,7 +3,7 @@
       double precision x,qtmin,qtmax,qt,jac
       double precision qtmin2,qtmax2,qt2
       double precision tiny
-      parameter (tiny=1D-5)
+      parameter (tiny=1d-5)
       double precision a,b,xx
 
       qtmin2 = qtmin**2
@@ -23,7 +23,7 @@
       implicit none
       double precision x,qtmin2,qtmax2,qt2,jac
       double precision tiny
-      parameter (tiny=1D-5)
+      parameter (tiny=1d-5)
       double precision a,b,xx
 
       a = 1d0/(1+log(qtmax2/tiny))
@@ -43,3 +43,61 @@
       jac=jac*(qtmax-qtmin)
       return
       end
+
+      subroutine qtweight_res(x,qtmin,qtmax,qt,jac)
+      implicit none
+      double precision x,qtmin,qtmax,qt,jac
+      double precision qtmin2,qtmax2,qt2
+      double precision tiny,esp
+      parameter (tiny=1d-3,esp=0.5d0)
+      double precision a,b,xx
+      a = log(qtmin/tiny)**esp
+      b = log(qtmax/tiny)**esp
+      xx= a + (b-a)*x
+      jac=jac*(b-a)
+      qt = tiny*exp(xx**(1d0/esp));
+      jac=jac*qt*(xx**(1d0/esp-1d0))/esp;
+      return
+      end
+      
+c     other options
+c  double tiny = 1E-5;
+c  double a = 1./(1+log(qtmx/tiny));
+c  double b = 1./(1+log(qtmn/tiny));
+c  double x2 = a + (b-a) * x[1];
+c  jac = jac * (b-a);
+c  double qt=tiny*exp(1./x2 - 1);
+c  jac=jac*qt/pow(x2,2);
+
+c  double exp = 1./3.a;
+c  double a = pow(qtmn,exp);
+c  double b = pow(qtmx,exp);
+c  double x2=a+(b-a)*x[1];
+c  jac=jac*(b-a);
+c  double qt = pow(x2,1./exp);
+c  jac=jac*pow(x2,1./exp-1)/exp;
+
+c  double a = log(qtmn);
+c  double b = log(qtmx);
+c  double x2=a+(b-a)*x[1];
+c  jac=jac*(b-a);
+c  double qt = exp(x2);
+c  jac=jac*qt;
+
+
+c  double tiny = 1E-3;
+c  double a = log(log(qtmn/tiny));
+c  double b = log(log(qtmx/tiny));
+c  double x2=a+(b-a)*x[1];
+c  jac=jac*(b-a);
+c  double qt = tiny*exp(exp(x2));
+c  jac=jac*qt*exp(x2);
+  
+c  double base = 100000.;
+c  double a = log(qtmn)/log(base);
+c  double b = log(qtmx)/log(base);
+c  double x2=a+(b-a)*x[1];
+c  jac=jac*(b-a);
+c  double qt = exp(x2*log(base));
+c  jac=jac*qt*log(base);
+      
