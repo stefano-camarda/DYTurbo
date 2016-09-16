377f360c3e2f7a8c63b367c5d76d05c94a1c4b48
Stefano Camarda <stefano.camarda@cern.ch>
2016-09-16 14:56:52 +0200

    fixes on loint

--- eba/loint.C
+++ eba/loint.C
@@ -16,10 +16,12 @@ double loint::lint(double costh, double m, double y, int mode)
   double exppy = exp(y);
   double expmy = 1./exppy;
 
+  /*
   if (y >= 0)
     phasespace::setcthbounds(opts.costhmin,opts.costhmax);
   else
     phasespace::setcthbounds(-opts.costhmax,-opts.costhmin);
+  */
   
   mesq::setpropagators(m);
 
@@ -55,13 +57,17 @@ double loint::lint(double costh, double m, double y, int mode)
   //     pick whatever value of phi and phi_lep
   double phi = 2*M_PI*0.25;
   double phi_lep = 0.75*2*M_PI;
-  phasespace::set_mqtycth(m, 0., y, costh);
-  genV4p(m, 0., y, phi);
-  genl4p(costh, phi_lep);
+  phasespace::set_mqtyphi(m, 0., y, phi);
+  phasespace::set_cth(costh);
+  phasespace::set_philep(phi_lep);
+  phasespace::calcexpy();
+  phasespace::calcmt();
+  phasespace::genV4p();
+  phasespace::genl4p();
 
   double p3[4], p4[4];
-  getp3(p3);
-  getp4(p4);
+  phasespace::getp3(p3);
+  phasespace::getp4(p4);
 
   double p1[4];
   p1[0] = 0.;
@@ -108,25 +114,63 @@ double loint::lint(double costh, double m, double y, int mode)
   initsigma_cpp_(m, cthmom0, cthmom1, cthmom2);
 
   double fbGeV2=0.38937966e12;
-  */
-  /*
-  cout << mesq::mesqij[0]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Ub][parton::U] << "  " << sigmaij_.sigmaij_[parton::ub][parton::u ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[1]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::U][parton::Ub] << "  " << sigmaij_.sigmaij_[parton::u ][parton::ub ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[2]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Db][parton::D] << "  " << sigmaij_.sigmaij_[parton::db][parton::d ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[3]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::D][parton::Db] << "  " << sigmaij_.sigmaij_[parton::d ][parton::db ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[4]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Sb][parton::S] << "  " << sigmaij_.sigmaij_[parton::sb][parton::s ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[5]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::S][parton::Sb] << "  " << sigmaij_.sigmaij_[parton::s ][parton::sb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[6]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Cb][parton::C] << "  " << sigmaij_.sigmaij_[parton::cb][parton::c ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[7]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::C][parton::Cb] << "  " << sigmaij_.sigmaij_[parton::c ][parton::cb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[8]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Bb][parton::B] << "  " << sigmaij_.sigmaij_[parton::bb][parton::b ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
-  cout << mesq::mesqij[9]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::B][parton::Bb] << "  " << sigmaij_.sigmaij_[parton::b ][parton::bb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+
+  cout << m << "  " << y << "  "  << costh << endl;  
+  fcomplex za[12][12];
+  fcomplex zb[12][12];
+  int N = 5;
+  spinoru_(N,p,za,zb);
+  cout << real(cx(za[2][1])*cx(zb[0][3]))/m/m << "  " << (1 + costh)/2 << endl;
+  cout << real(cx(za[2][0])*cx(zb[1][3]))/m/m << "  " << (1 - costh)/2 <<  endl;
+  if (opts.nproc == 3)
+    {
+      cout << mesq::mesqij[0]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Ub][parton::U] << "  " << sigmaij_.sigmaij_[parton::ub][parton::u ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[1]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::U][parton::Ub] << "  " << sigmaij_.sigmaij_[parton::u ][parton::ub ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[2]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Db][parton::D] << "  " << sigmaij_.sigmaij_[parton::db][parton::d ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[3]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::D][parton::Db] << "  " << sigmaij_.sigmaij_[parton::d ][parton::db ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[4]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Sb][parton::S] << "  " << sigmaij_.sigmaij_[parton::sb][parton::s ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[5]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::S][parton::Sb] << "  " << sigmaij_.sigmaij_[parton::s ][parton::sb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[6]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Cb][parton::C] << "  " << sigmaij_.sigmaij_[parton::cb][parton::c ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[7]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::C][parton::Cb] << "  " << sigmaij_.sigmaij_[parton::c ][parton::cb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[8]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Bb][parton::B] << "  " << sigmaij_.sigmaij_[parton::bb][parton::b ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+      cout << mesq::mesqij[9]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::B][parton::Bb] << "  " << sigmaij_.sigmaij_[parton::b ][parton::bb ]* M_PI*6./fbGeV2*(2*m2) << endl;//* M_PI*6./fbGeV2*(2*m2) << endl;
+    }
+  else if (opts.nproc == 1)
+    {
+      cout << mesq::mesqij[0 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Db][parton::U ] << endl;
+      cout << mesq::mesqij[1 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::U ][parton::Db] << endl;
+      cout << mesq::mesqij[2 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Sb][parton::U ] << endl;
+      cout << mesq::mesqij[3 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::U ][parton::Sb] << endl;
+      cout << mesq::mesqij[4 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Bb][parton::U ] << endl;
+      cout << mesq::mesqij[5 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::U ][parton::Bb] << endl;
+      cout << mesq::mesqij[6 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Sb][parton::C ] << endl;
+      cout << mesq::mesqij[7 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::C ][parton::Sb] << endl;
+      cout << mesq::mesqij[8 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Db][parton::C ] << endl;
+      cout << mesq::mesqij[9 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::C ][parton::Db] << endl;
+      cout << mesq::mesqij[10]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Bb][parton::C ] << endl;
+      cout << mesq::mesqij[11]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::C ][parton::Bb] << endl;
+    }
+  else if (opts.nproc == 2)
+    {
+      cout << mesq::mesqij[0 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Ub][parton::D ] << endl;
+      cout << mesq::mesqij[1 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::D ][parton::Ub] << endl;
+      cout << mesq::mesqij[2 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Ub][parton::S ] << endl;
+      cout << mesq::mesqij[3 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::S ][parton::Ub] << endl;
+      cout << mesq::mesqij[4 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Ub][parton::B ] << endl;
+      cout << mesq::mesqij[5 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::B ][parton::Ub] << endl;
+      cout << mesq::mesqij[6 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Cb][parton::S ] << endl;
+      cout << mesq::mesqij[7 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::S ][parton::Cb] << endl;
+      cout << mesq::mesqij[8 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Cb][parton::D ] << endl;
+      cout << mesq::mesqij[9 ]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::D ][parton::Cb] << endl;
+      cout << mesq::mesqij[10]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::Cb][parton::B ] << endl;
+      cout << mesq::mesqij[11]* M_PI*6./fbGeV2*(2*m2) << "  " << msqc[parton::B ][parton::Cb] << endl;
+    }
   cout << endl;
-  */
-//     End of ME check
-//*******************************************************
 
   
-  
+  //     End of ME check
+  //*******************************************************
+  */
   
   //Set factorization scale
   double muf;
@@ -176,8 +220,8 @@ double loint::lint(double costh, double m, double y, int mode)
       xmsq += real(mesq::mesqij[3]*fx1[parton::Ub]*fx2[parton::S ]);
       xmsq += real(mesq::mesqij[4]*fx1[parton::B ]*fx2[parton::Ub]);
       xmsq += real(mesq::mesqij[5]*fx1[parton::Ub]*fx2[parton::B ]);
-      xmsq += real(mesq::mesqij[6]*fx1[parton::C ]*fx2[parton::Sb]);
-      xmsq += real(mesq::mesqij[7]*fx1[parton::Sb]*fx2[parton::C ]);
+      xmsq += real(mesq::mesqij[6]*fx1[parton::S ]*fx2[parton::Cb]);
+      xmsq += real(mesq::mesqij[7]*fx1[parton::Cb]*fx2[parton::S ]);
       xmsq += real(mesq::mesqij[8]*fx1[parton::D ]*fx2[parton::Cb]);
       xmsq += real(mesq::mesqij[9]*fx1[parton::Cb]*fx2[parton::D ]);
       xmsq += real(mesq::mesqij[10]*fx1[parton::B ]*fx2[parton::Cb]);
