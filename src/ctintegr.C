04fc0e24a1a46487a01e7323b2e877c5a7f30d41
Stefano Camarda <stefano.camarda@cern.ch>
2016-09-19 01:07:50 +0200

    new ps gen for ct MC

--- src/ctintegr.C
+++ src/ctintegr.C
@@ -60,21 +60,22 @@ integrand_t ctintegrandMC(const int &ndim, const double x[], const int &ncomp, d
   //Jacobian of the change of variables from the unitary hypercube x[6] to the m, y, qt, phi, costh, philep boundaries
   double jac = 1.;
   
-  /************************
   bool status = true;
 
   double r1[2] = {x[0], x[1]};
-  status = phasespace::gen_my(r1, jac, true, true);
+  status = phasespace::gen_my(r1, jac, true, !opts.fixedorder);  //qtcut = true, qtswitching = true
   if (!status)
     {
       f[0] = 0.;
       return 0;
     }
 
+  double qt;
   //In the fixed order calculation, integrate from qtcut to infinity
   if (opts.fixedorder)
     {
       phasespace::gen_qt_ctfo(x[2], jac);
+      qt = phasespace::qt;
       phasespace::set_qt(0.); //In the fixed order calculation evaluate kinematic cuts with qt=0
     }
   else
@@ -93,8 +94,9 @@ integrand_t ctintegrandMC(const int &ndim, const double x[], const int &ncomp, d
 	  f[0] = 0.;
 	  return 0;
 	}
+      qt = phasespace::qt;
     }
-  jac = jac *2.*phasespace::qt;
+  jac = jac *2.*qt;
 
   //generate boson 4-momentum, with m, qt, y, and phi
   phasespace::set_phiV(-M_PI+2.*M_PI*x[5]);
@@ -129,204 +131,9 @@ integrand_t ctintegrandMC(const int &ndim, const double x[], const int &ncomp, d
   /////////////////////
 
   double m = phasespace::m;
-  double qt = phasespace::qt;
   double y = phasespace::y;
 
   jac = jac/2./M_PI;
-  ************************/
-
-  /************************/
-  // Generate the boson invariant mass between the integration boundaries
-  double mcut = (opts.xqtcut != 0.) ? phasespace::qtmax/opts.xqtcut : phasespace::mmax;
-  double wsqmin = pow(phasespace::mmin,2);
-  double wsqmax = pow(min(phasespace::mmax,mcut),2);
-  if (wsqmin >= wsqmax)
-    {
-      f[0]=0.;
-      return 0;
-    }
-  double x1=x[0];
-  double m2,wt;
-  breitw_(x1,wsqmin,wsqmax,opts.rmass,opts.rwidth,m2,wt);
-  double m=sqrt(m2);
-  jac=jac*wt;
-
-  //Limit y boundaries to the kinematic limit in y
-  double ylim = 0.5*log(pow(energy_.sroot_,2)/m2);
-  double ymn = min(max(-ylim, phasespace::ymin),ylim);
-  double ymx = max(min(ylim, phasespace::ymax),-ylim);
-  if (ymn >= ymx)
-    {
-      f[0]=0.;
-      return 0;
-    }
-
-  //integrate between ymin and ymax
-  double y=ymn+(ymx-ymn)*x[1];
-  jac=jac*(ymx-ymn);
-  
-  //integrate between qtmin and qtmax
-  //use qt2 to get the correct jacobian!
-
-  //In the fixed order calculation, integrate from qtcut to infinity
-  double expy, expmy;
-  double qtmn, qtmx;
-  double qtcut = max(opts.qtcut,opts.xqtcut*m);
-  if (opts.fixedorder)
-    {
-      if (phasespace::qtmin > 0)
-	{
-	  f[0]=0.;
-	  return 0;
-	}
-      qtmn = qtcut;
-      qtmx = 1e10;
-    }
-  else
-    {
-      expy = exp(y);
-      expmy = exp(-y);
-      qtmn = max(qtcut, phasespace::qtmin);
-      double cosh2y34=pow((expy+expmy)*0.5,2);
-      double kinqtlim = sqrt(pow(pow(energy_.sroot_,2)+m*m,2)/(4*pow(energy_.sroot_,2)*cosh2y34)-m*m);
-      double switchqtlim = switching::qtlimit(m);
-      double qtlim = min(kinqtlim, switchqtlim);
-      qtmx = min(qtlim, phasespace::qtmax);
-      if (qtmn >= qtmx)
-	{
-	  f[0]=0.;
-	  return 0;
-	}
-    }
-  double qtmn2 = pow(qtmn,2);
-  double qtmx2 = pow(qtmx,2);
-  
-  double tiny = 1E-5;
-  double a = 1./(1+log(qtmx2/tiny));
-  double b = 1./(1+log(qtmn2/tiny));
-  double x2 = a + (b-a) * x[2];
-  jac = jac * (b-a);
-  double qt2=tiny*exp(1./x2 - 1);
-  jac=jac*qt2/pow(x2,2);
-  
-  double qt=sqrt(qt2);
-
-  // First lepton direction: Cos of the polar angle
-  double costh=-1.+2.*x[3];
-  jac=jac*2.;
-  
-  //Generate vector boson and lepton azimuthal angles 
-  double phi = 2.*M_PI*x[4];
-  double phi_lep = 2.*M_PI*x[5];
-      
-  double p3[4];
-  double p4[4];
-  double costh_CS;
-  if (opts.fixedorder)
-    {
-      double pV[4];
-      pV[0]=0.;
-      pV[1]=0.;
-      pV[2]=0.5*m*(expy-expmy);
-      pV[3]=0.5*m*(expy+expmy);
-
-      // momentum of the first lepton 
-      double p4cm[4];
-      p4cm[3]=m/2.;
-      p4cm[0]=p4cm[3]*sin(acos(costh))*sin(phi_lep);
-      p4cm[1]=p4cm[3]*sin(acos(costh))*cos(phi_lep);
-      p4cm[2]=p4cm[3]*costh;
-
-      // Boost to go in the Lab frame
-      boost_(m,pV,p4cm,p4);
-
-      //  momentum of the second lepton
-      p3[3]=pV[3]-p4[3];
-      p3[0]=pV[0]-p4[0];
-      p3[1]=pV[1]-p4[1];
-      p3[2]=pV[2]-p4[2];
-
-      //apply lepton cuts
-      if (opts.makelepcuts)
-	if (!cuts::lep(p3, p4))
-	  {
-	    f[0]=0.;
-	    return 0;
-	  }
-      costh_CS = costh;
-    }
-  else
-    {
-      //  vector boson momentum: pV(4)^2-pV(1)^2-pV(2)^2-pV(3)^2=m2
-      double mt2=m2+qt2;
-      double pV[4];
-      pV[0]=qt*cos(phi);
-      pV[1]=qt*sin(phi);
-      pV[2]=0.5*sqrt(mt2)*(expy-expmy);
-      pV[3]=0.5*sqrt(mt2)*(expy+expmy);
-
-      // momentum of the first lepton 
-      double p4cm[4];
-      p4cm[3]=m/2.;
-      p4cm[0]=p4cm[3]*sin(acos(costh))*sin(phi_lep);
-      p4cm[1]=p4cm[3]*sin(acos(costh))*cos(phi_lep);
-      p4cm[2]=p4cm[3]*costh;
-
-      // Boost to go in the Lab frame
-      boost_(m,pV,p4cm,p4);
-
-      //  momentum of the second lepton
-      p3[3]=pV[3]-p4[3];
-      p3[0]=pV[0]-p4[0];
-      p3[1]=pV[1]-p4[1];
-      p3[2]=pV[2]-p4[2];
-
-      //apply lepton cuts
-      if (opts.makelepcuts)
-	if (!cuts::lep(p3, p4))
-	  {
-	    f[0]=0.;
-	    return 0;
-	  }
-  			    
-      //Calculate costh according to a qt-recoil prescription
-      double kt1,kt2;
-  
-      //CS frame prescription
-      if (opts.qtrec_cs)
-	{
-	  kt1 = pV[0]/2.;
-	  kt2 = pV[1]/2.;
-	}
-
-      //MY (DYRES) prescription
-      if (opts.qtrec_naive)
-	{
-	  kt1=(1.+pV[2]/(sqrt(m2)+pV[3]))*pV[0]/2;
-	  kt2=(1.+pV[2]/(sqrt(m2)+pV[3]))*pV[1]/2;
-	}  
-
-      //alternative k1t = 0 prescription
-      if (opts.qtrec_kt0)
-	{
-	  kt1 = 0;
-	  kt2 = 0;
-	}
-
-      double zeta1=1./m2/2.*(m2+2.*(pV[0]*kt1+pV[1]*kt2)+sqrt(pow((m2+2.*(pV[0]*kt1+pV[1]*kt2)),2)-4.*mt2*(pow(kt1,2)+pow(kt2,2))));
-
-      double qP1=(pV[3]-pV[2])*energy_.sroot_/2.;
-      double qP2=(pV[3]+pV[2])*energy_.sroot_/2.;
-  
-      double kap1[4];
-      kap1[3]=energy_.sroot_/2.*(zeta1*m2/2./qP1+(pow(kt1,2)+pow(kt2,2))/zeta1*qP1/m2/pow(energy_.sroot_,2)*2.);
-      kap1[0]=kt1;
-      kap1[1]=kt2;
-      kap1[2]=energy_.sroot_/2.*(zeta1*m2/2./qP1-(pow(kt1,2)+pow(kt2,2))/zeta1*qP1/m2/pow(energy_.sroot_,2)*2.);
-
-      costh_CS=1.-4.*(kap1[3]*p4[3]-kap1[2]*p4[2]-kap1[1]*p4[1]-kap1[0]*p4[0])/m2;
-    }
-  /************************/
 
   //apply resummation switching
   double swtch = switching::swtch(qt, m);
@@ -365,7 +172,7 @@ integrand_t ctintegrandMC(const int &ndim, const double x[], const int &ncomp, d
       {
 	double wt = weight*f[i];
 	hists_setpdf_(&i);
-	//hists_fill_(p3, p4, &wt);
+	hists_fill_(phasespace::p3, phasespace::p4, &wt);
       }
 
   end_time = clock();
@@ -404,17 +211,19 @@ integrand_t ctintegrand3d(const int &ndim, const double x[], const int &ncomp, d
   bool status = true;
 
   double r2[2] = {x[0], x[1]};
-  status = phasespace::gen_my(r2, jac, true, true);
+  status = phasespace::gen_my(r2, jac, true, !opts.fixedorder);  //qtcut = true, qtswitching = true
   if (!status)
     {
       f[0] = 0.;
       return 0;
     }
 
+  double qt;
   //In the fixed order calculation, integrate from qtcut to infinity
   if (opts.fixedorder)
     {
       phasespace::gen_qt_ctfo(x[2], jac);
+      qt = phasespace::qt;
       phasespace::set_qt(0.); //In the fixed order calculation evaluate kinematic cuts with qt=0
     }
   else
@@ -433,13 +242,13 @@ integrand_t ctintegrand3d(const int &ndim, const double x[], const int &ncomp, d
 	  f[0] = 0.;
 	  return 0;
 	}
+      qt = phasespace::qt;
     }
-  jac = jac *2.*phasespace::qt;
+  jac = jac *2.*qt;
 
   phasespace::set_phiV(0.);
   
   double m = phasespace::m;
-  double qt = phasespace::qt;
   double y = phasespace::y;
 
   //generate boson 4-momentum, with m, qt, y and phi=0
@@ -471,13 +280,14 @@ integrand_t ctintegrand3d(const int &ndim, const double x[], const int &ncomp, d
   //avoid nans
   if (isnan_ofast(f[0]))
     {
+      cout << "nan in ctintegrand3d" << endl;
       f[0]=0.;
       if (opts.PDFerrors)
 	for (int i = 1; i < opts.totpdf; i++)
 	  f[i] = 0.;
       return 0;
     }
-	   
+
   f[0] = f[0]*jac; //*swtch; switching function is inside ctint_
   if (opts.PDFerrors)
     for (int i = 1; i < opts.totpdf; i++)
@@ -546,7 +356,7 @@ integrand_t ctintegrand2d(const int &ndim, const double x[], const int &ncomp, d
   bool status = true;
 
   double r2[2] = {x[0], x[1]};
-  status = phasespace::gen_my(r2, jac, true, true);
+  status = phasespace::gen_my(r2, jac, true, !opts.fixedorder);  //qtcut = true, qtswitching = true
   if (!status)
     {
       f[0] = 0.;
