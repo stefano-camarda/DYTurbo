ebca3f2fa38bd924d51c0bc505e2de00ebeefa68
Jakub <jakub.cuth@cern.ch>
2016-10-05 03:00:32 +0200

    additional tests on kinematic cuts

--- test/Cuts_unittest.cxx
+++ test/Cuts_unittest.cxx
@@ -15,14 +15,31 @@
 #include "histo/Kinematics.h"
 #include "histo/KinematicCuts.h"
 
+#include "src/old_cuts.h"
+//#include "src/old_cuts.C"
+
 TEST(CutInterface,StandardCuts){
     double l1[] = {1., 1., 1., sqrt(3)};
     opts.makecuts = true;
+    opts.lptcut = 0;
+    opts.lycut = 1000;
     opts.l1ptcut = 10;
     opts.l2ptcut = 10;
+    opts.l1ycut = 1000;
+    opts.l2ycut = 1000;
     ASSERT_EQ(Kinematics::Cuts::SkipEvent, Kinematics::Cuts::KeepThisEvent(l1,l1));
     double l2[] = {100., 100., 100., 100.*sqrt(3)};
     ASSERT_EQ(Kinematics::Cuts::KeepEvent, Kinematics::Cuts::KeepThisEvent(l2,l2));
 }
+
+
+TEST(CutInterface,CrossCheckWithPrevious){
+    double l1[] = {1., 1., 1., sqrt(3)};
+    ASSERT_EQ(cuts::lep(l1,l1), Kinematics::Cuts::KeepThisEvent(l1,l1));
+    double l2[] = {100., 100., 100., 100.*sqrt(3)};
+    ASSERT_EQ(cuts::lep(l2,l2), Kinematics::Cuts::KeepThisEvent(l2,l2));
+}
+
+
 
 #endif /* ifndef Cuts_unittest_CXX */
