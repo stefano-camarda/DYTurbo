c8c010929364087d025fb704e84a707575fd6cc2
Stefano Camarda <stefano.camarda@cern.ch>
2016-10-13 18:05:34 +0200

    separate dyres fortran interface

--- resum/resint.C
+++ resum/resint.C
@@ -1,6 +1,7 @@
 #include "resint.h"
 #include "settings.h"
-#include "interface.h"
+//#include "interface.h"
+#include "dyres_interface.h"
 #include "resconst.h"
 #include "mesq.h"
 #include "hcoefficients.h"
