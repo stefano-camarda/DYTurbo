e5caff66d9eda4da3fb3525e17431e470e9c4f94
Stefano Camarda <stefano.camarda@cern.ch>
2016-10-20 14:43:17 +0200

    support for ai integration

--- src/bornintegr.C
+++ src/bornintegr.C
@@ -6,8 +6,8 @@
 #include "cubacall.h"
 //#include "isnan.h"
 
-//#include <iostream>
-//#include <iomanip>
+#include <iostream>
+#include <iomanip>
 //#include <ctime>
 //#include <math.h>
 
@@ -37,14 +37,12 @@ integrand_t lointegrandMC(const int &ndim, const double x[], const int &ncomp, d
 			 double &weight, const int &iter)
 {
   clock_t begin_time, end_time;
-
   begin_time = clock();
 
+  f[0]=0.;
+  
   if (opts.fixedorder && phasespace::qtmin > 0)
-    {
-      f[0]=0.;
-      return 0;
-    }
+    return 0;
   
   //Jacobian of the change of variables from the unitary hypercube x[3] to the m, y boundaries
   double jac = 1.;
@@ -54,10 +52,8 @@ integrand_t lointegrandMC(const int &ndim, const double x[], const int &ncomp, d
   double r1[2] = {x[0], x[1]};
   status = phasespace::gen_my(r1, jac);
   if (!status)
-    {
-      f[0] = 0.;
-      return 0;
-    }
+    return 0;
+
   phasespace::set_qt(0.);
   phasespace::set_phiV(0.);
   
@@ -72,20 +68,28 @@ integrand_t lointegrandMC(const int &ndim, const double x[], const int &ncomp, d
 
   //apply cuts
   if (!Kinematics::Cuts::KeepThisEvent(phasespace::p3,phasespace::p4))
-    {
-      f[0] = 0.;
-      return 0;
-    }
+    return 0;
   
   //calculate integrand
-  f[0] = loint::lint(phasespace::costh,phasespace::m,phasespace::y,0)*jac;
-
+  double f2[2];
+  loint::lint(phasespace::costh,phasespace::m,phasespace::y,0,f2);
+  f[0] = f2[0]*jac;
+  
   //fill histograms
   if (iter==last_iter)
     {
       double wt = weight*f[0];
       hists_fill_(phasespace::p3,phasespace::p4,&wt);
     }
+
+  end_time = clock();
+  if (opts.timeprofile)
+    cout << setw (3) << "m" << setw(10) << phasespace::m
+	 << setw(4) << "qt" << setw(10) <<  phasespace::qt
+      	 << setw(4) << "y" << setw(10) <<  phasespace::y
+	 << setw(8) << "result" << setw(10) << f[0]
+	 << setw(10) << "tot time" << setw(10) << float( end_time - begin_time ) /  CLOCKS_PER_SEC
+	 << endl;
   
   tell_to_grid_we_are_alive();
   return 0;
@@ -121,14 +125,14 @@ int lointegrand2d_cubature(unsigned ndim, const double x[], void *data, unsigned
 integrand_t lointegrand2d(const int &ndim, const double x[], const int &ncomp, double f[])
 {
   clock_t begin_time, end_time;
-
   begin_time = clock();
 
+  f[0]=0.;
+  if (opts.helicity >= 0)
+    f[1]=0.;
+  
   if (opts.fixedorder && phasespace::qtmin > 0)
-    {
-      f[0]=0.;
-      return 0;
-    }
+    return 0;
 
   //Jacobian of the change of variables from the unitary hypercube x[3] to the m, y boundaries
   double jac = 1.;
@@ -137,19 +141,33 @@ integrand_t lointegrand2d(const int &ndim, const double x[], const int &ncomp, d
   double r1[2] = {x[0], x[1]};
   status = phasespace::gen_my(r1, jac);
   if (!status)
-    {
-      f[0] = 0.;
-      return 0;
-    }
+    return 0;
+
   phasespace::set_qt(0.);
 
   double m = phasespace::m;
   double y = phasespace::y;
 
-  f[0] = loint::lint(0.,phasespace::m,phasespace::y,1)*jac;
+  double f2[2];
+  loint::lint(0.,phasespace::m,phasespace::y,1,f2);
 
+  f[0] = f2[0]*jac;
+  if (opts.helicity >= 0)
+    f[1] = f2[1]*jac;
+  
   //perform phi integration
   f[0] = f[0] * 2.*M_PI;
+  if (opts.helicity >= 0)
+    f[1]*=2.*M_PI;
+
+  end_time = clock();
+  if (opts.timeprofile)
+    cout << setw (3) << "m" << setw(10) << phasespace::m
+	 << setw(4) << "qt" << setw(10) <<  phasespace::qt
+      	 << setw(4) << "y" << setw(10) <<  phasespace::y
+	 << setw(8) << "result" << setw(10) << f[0]
+	 << setw(10) << "tot time" << setw(10) << float( end_time - begin_time ) /  CLOCKS_PER_SEC
+	 << endl;
   
   tell_to_grid_we_are_alive();
   return 0;
