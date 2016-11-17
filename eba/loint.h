57ada6845b19b58b6eb47d541b6a4a4ab34f1953
Stefano Camarda <stefano.camarda@cern.ch>
2016-11-17 13:57:59 +0100

    moved alfa beta integration to a common namespace

--- eba/loint.h
+++ eba/loint.h
@@ -5,7 +5,6 @@
 
 namespace loint
 {
-  void init();
   //  double lint(double costh, double m, double y, int mode, double f[]);
   void lint(double costh, double m, double y, int mode, double f[2]);
   double convolute(double fx1[2*MAXNF+1], double fx2[2*MAXNF+1], complex <double> mesq[12]);
