501117aa65c120607f407a89614331a2f275f07e
Stefano Camarda <stefano.camarda@cern.ch>
2016-11-30 01:18:04 +0100

    alfa beta integration rule options

--- src/abint.C
+++ src/abint.C
@@ -13,7 +13,7 @@ void abint::init()
   //initialize the points of the gaussian quadrature for the alfa and beta integration
   int abintervals = 1; //--> make a setting
   int abrule = 64;     //--> make a setting
-  abdim = abintervals*abrule;
+  abdim = opts.abintervals*opts.abrule;
 
   abx = new double [abdim];
   abw = new double [abdim];
@@ -25,19 +25,19 @@ void abint::init()
   double max = 1.;
   double lmm = log(max/min);
     
-  for (int i = 0; i < abintervals; i++)
+  for (int i = 0; i < opts.abintervals; i++)
     {
-      double a = min+(max-min)*i/abintervals;
-      double b = min+(max-min)*(i+1)/abintervals;
+      double a = min+(max-min)*i/opts.abintervals;
+      double b = min+(max-min)*(i+1)/opts.abintervals;
       double c = 0.5*(a+b);
       double m = 0.5*(b-a);
-      for (int j = 0; j < abrule; j++)
+      for (int j = 0; j < opts.abrule; j++)
 	{
-	  double x = c+m*gr::xxx[abrule-1][j];
+	  double x = c+m*gr::xxx[opts.abrule-1][j];
 	  double t = min*pow(max/min,x);
 	  double jac=t*lmm;
-	  abx[j+i*abrule]=t;
-	  abw[j+i*abrule]=gr::www[abrule-1][j]*m*jac;
+	  abx[j+i*opts.abrule]=t;
+	  abw[j+i*opts.abrule]=gr::www[opts.abrule-1][j]*m*jac;
 	}
     }
 }
