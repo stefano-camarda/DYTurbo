bcd249d296955bb7f9602e8d8a76be95aa35fb2a
Stefano Camarda <stefano.camarda@cern.ch>
2018-06-06 01:31:08 +0200

    removed gotos

--- mcfm/transform.f
+++ mcfm/transform.f
@@ -18,11 +18,12 @@
      . k(4),kt(4),ks(4),kDk,ksDks,kDp(3:mxpart),ksDp(3:mxpart)
       integer ip,kp,j,nu,jp,ipart
 
-      do j=1,npart+2
-      do nu=1,4
-        q(j,nu)=0d0
-      enddo
-      enddo
+      q=0d0
+c      do j=1,npart+2
+c      do nu=1,4
+c        q(j,nu)=0d0
+c      enddo
+c      enddo
 
       if ((ip .le. 2) .and. (kp .le. 2)) then
 c---initial-initial
@@ -39,40 +40,36 @@ c---initial-initial
 
         ipart=3
         do j=3,npart+2
-        if (j .eq. jp) then
-            go to 19
-        else
-            kDp(j)=k(4)*p(j,4)-k(1)*p(j,1)-k(2)*p(j,2)-k(3)*p(j,3)
-            ksDp(j)=ks(4)*p(j,4)-ks(1)*p(j,1)-ks(2)*p(j,2)-ks(3)*p(j,3)
-            do nu=1,4
-              q(ipart,nu)=p(j,nu)-two*ksDp(j)*ks(nu)/ksDks
-     .        +two*kDp(j)*kt(nu)/kDk
-            enddo
-        ipart=ipart+1
-        endif
- 19     continue
+           if (j .eq. jp) cycle
+           kDp(j)=k(4)*p(j,4)-k(1)*p(j,1)-k(2)*p(j,2)-k(3)*p(j,3)
+           ksDp(j)=ks(4)*p(j,4)-ks(1)*p(j,1)-ks(2)*p(j,2)-ks(3)*p(j,3)
+c           do nu=1,4
+c              q(ipart,nu)=p(j,nu)-two*ksDp(j)*ks(nu)/ksDks
+c     .             +two*kDp(j)*kt(nu)/kDk
+c           enddo
+           q(ipart,:)=p(j,:)-two*ksDp(j)*ks(:)/ksDks
+     .          +two*kDp(j)*kt(:)/kDk
+           ipart=ipart+1
         enddo
+        
         return
       elseif (((ip .le. 2) .and. (kp .gt. 2)) .or.
      .        ((ip .gt. 2) .and. (kp .le. 2))) then
 c---initial-final or final-initial
         ipart=1
         omx=one-x
-           do j=1,npart+2
-                do nu=1,4
-                   if (j.eq.ip) then
-                   q(ipart,nu)=x*p(ip,nu)
-                   elseif (j.eq.jp) then
-                   goto 20
-                   elseif (j.eq.kp) then
-                   q(ipart,nu)=p(jp,nu)+p(kp,nu)+omx*p(ip,nu)
-                   else
-                   q(ipart,nu)=p(j,nu)
-                   endif
-                enddo
-                ipart=ipart+1
- 20             continue
-           enddo
+        do j=1,npart+2
+           if (j .eq. jp) then
+              cycle
+           elseif (j.eq.ip) then
+              q(ipart,:)=x*p(ip,:)
+           elseif (j.eq.kp) then
+              q(ipart,:)=p(jp,:)+p(kp,:)+omx*p(ip,:)
+           else
+              q(ipart,:)=p(j,:)
+           endif
+           ipart=ipart+1
+        enddo
         return
       elseif ((ip .gt. 2) .and. (kp .gt. 2)) then
 c---final-final
