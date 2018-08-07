ff425e65527c028c72103145c9b99ba714310f14
Stefano Camarda <stefano.camarda@cern.ch>
2018-08-08 01:26:17 +0200

    interface for bessel functions

--- resum/bessel.f
+++ resum/bessel.f
@@ -9,3 +9,30 @@
       fort_besy0 = dbesy0(x)
       return
       end
+
+      function fort_besj1(x)
+      double precision x,fort_besj1
+      fort_besj1 = dbesj1(x)
+      return
+      end
+      
+      function fort_besy1(x)
+      double precision x,fort_besy1
+      fort_besy1 = dbesy1(x)
+      return
+      end
+
+      function fort_besjn(n,x)
+      double precision x,fort_besjn
+      integer n
+      fort_besjn = dbesjn(n,x)
+      return
+      end
+
+      function fort_besyn(n,x)
+      double precision x,fort_besyn
+      integer n
+      fort_besyn = dbesyn(n,x)
+      return
+      end
+      
