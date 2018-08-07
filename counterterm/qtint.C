349c943797c72be9d44ff4742d74ef788d701415
Stefano Camarda <stefano.camarda@cern.ch>
2018-08-08 00:57:30 +0200

    analytic qt integration

--- counterterm/qtint.C
+++ counterterm/qtint.C
@@ -6,8 +6,10 @@
 #include "omegaintegr.h"
 #include "gaussrules.h"
 #include "switch.h"
+#include "itilde.h"
 #include "dyres_interface.h"
 #include "numbers.h"
+#include <iostream>
 
 double qtint::LL1_mesqij[12];
 double qtint::LL2_mesqij[12];
@@ -41,6 +43,23 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
       double swtch = switching::swtch(qt, m);
       //xmio is used in besselkfast for Itilde
       xmio_.xmio_ = sqrt(qt2/(q2/pow(a_param_.a_param_,2)));
+
+      /*
+      cout << endl;
+      cout << " qt " << qt  << " m " << m << endl;
+      cout << " fortran itilde(1) " << itilde_(one) << endl;
+      itilde::calc(qt,m/a_param_.a_param_,1);
+      //cout << " C++ itilde(1) " << itilde::calc(qt,m/a_param_.a_param_,1)
+      cout << " fortran itilde(2) " << itilde_(two) << endl;
+      itilde::calc(qt,m/a_param_.a_param_,2);
+      //cout << " C++ itilde(2) " << itilde::calc(qt,m/a_param_.a_param_,2)
+      cout << " fortran itilde(3) " << itilde_(three) << endl;
+      itilde::calc(qt,m/a_param_.a_param_,3);
+      //<< " C++ itilde(3) " << itilde::calc(qt,m/a_param_.a_param_,3)
+      cout << " fortran itilde(4) " << itilde_(four) << endl;
+      itilde::calc(qt,m/a_param_.a_param_,4);
+      //<< " C++ itilde(4) " << itilde::calc(qt,m/a_param_.a_param_,4)
+      */
       
       double LL1 = itilde_(one)/pow(q2,2)*pow(a_param_.a_param_,2)*swtch;
       double LL2 = itilde_(two)/pow(q2,2)*pow(a_param_.a_param_,2)*swtch;
@@ -55,7 +74,8 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
 	}
       return;
     }
-  
+
+  // mode == 2: qt-integrated
   
   //For fixed order predictions the counterterm is always evaluated at qt=0
   //Also when no cuts on the leptons are required, the cthmom are independent of pt, and so are the mesqij
@@ -68,10 +88,51 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
       omegaintegr::cthmoments(cthmom0, cthmom1, cthmom2);
       mesq::setmesq(cthmom0, cthmom1, cthmom2);
     }
-      
+
+  //In the fixed order case we just need to calculate the integral over qt from qtcut to infinity
+  //of Eq.(112) in https://arxiv.org/pdf/hep-ph/0508068.pdf:
+  //int_qtcut^inf dqt Itilde_n(qt/Q) = Q^2*qtcut/2 int_0^inf db J1(b*qtcut) ln^n(Q^2 b^2 / b0^2 + 1)
+  if (opts.fixedorder)
+    {
+      double LL1int = -2.*qtmin*itilde::integral(qtmin,m/a_param_.a_param_,1)/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL2int = -2.*qtmin*itilde::integral(qtmin,m/a_param_.a_param_,2)/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL3int = -2.*qtmin*itilde::integral(qtmin,m/a_param_.a_param_,3)/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL4int = -2.*qtmin*itilde::integral(qtmin,m/a_param_.a_param_,4)/pow(q2,2)*pow(a_param_.a_param_,2);
+
+      //loop on born subprocesses, i.e. born incoming partons ij
+      for (int sp = 0; sp < mesq::totpch; sp++)
+	{
+	  LL1_mesqij[sp] += LL1int*real(mesq::mesqij[sp]);
+	  LL2_mesqij[sp] += LL2int*real(mesq::mesqij[sp]);
+	  LL3_mesqij[sp] += LL3int*real(mesq::mesqij[sp]);
+	  LL4_mesqij[sp] += LL4int*real(mesq::mesqij[sp]);
+	}
+      return;
+    }
+  /*  //In the resummation case the integral can be performed if qtmax < dampk*m (the result is unstable for qt >~ 500)
+  else if (!opts.makecuts && qtmax < m*opts.dampk && qtmax < m*5)
+    {
+      double LL1int = 2.*(qtmax*itilde::integral(qtmax,m/a_param_.a_param_,1) - qtmin*itilde::integral(qtmin,m/a_param_.a_param_,1))/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL2int = 2.*(qtmax*itilde::integral(qtmax,m/a_param_.a_param_,2) - qtmin*itilde::integral(qtmin,m/a_param_.a_param_,2))/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL3int = 2.*(qtmax*itilde::integral(qtmax,m/a_param_.a_param_,3) - qtmin*itilde::integral(qtmin,m/a_param_.a_param_,3))/pow(q2,2)*pow(a_param_.a_param_,2);
+      double LL4int = 2.*(qtmax*itilde::integral(qtmax,m/a_param_.a_param_,4) - qtmin*itilde::integral(qtmin,m/a_param_.a_param_,4))/pow(q2,2)*pow(a_param_.a_param_,2);
+
+      //loop on born subprocesses, i.e. born incoming partons ij
+      for (int sp = 0; sp < mesq::totpch; sp++)
+	{
+	  LL1_mesqij[sp] += LL1int*real(mesq::mesqij[sp]);
+	  LL2_mesqij[sp] += LL2int*real(mesq::mesqij[sp]);
+	  LL3_mesqij[sp] += LL3int*real(mesq::mesqij[sp]);
+	  LL4_mesqij[sp] += LL4int*real(mesq::mesqij[sp]);
+	}
+      return;
+    }
+  */
+
+  //In the general case (resummation, either with cuts or at high pT) use a gaussian quadrature integration
   double qtmin2 = pow(qtmin,2);
   double qtmax2 = pow(qtmax,2);
-  double tiny = 1e-5;
+  double tiny = 1e-5; //be carefull, if qtmin^2 < tiny the phase space generation is screwed up
   double qta = 1./(1.+log(qtmax2/tiny));
   double qtb = 1./(1.+log(qtmin2/tiny));
   double min = 0;
@@ -91,6 +152,9 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
 	  double w = gr::www[opts.qtrule-1][j]*xm*jac;
 
 	  double qt = sqrt(qt2);
+	  //Here there should be a jacobian: jac=jac/qt/2.;
+	  //However since the original formulation of the integral is in dqt2,
+	  //the integral in dqt gets a factor 2*qt from the change of variable which cancels exactly the Jacobian
 
 	  double swtch = switching::swtch(qt, m);
 	  //if(switch.le.0.01d0) cycle ! do not apply this cut to avoid discontinuities. Instead the phase space is limited to qt and m switching limits
@@ -103,7 +167,7 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
 	  double LL3 = itilde_(three)/pow(q2,2)*pow(a_param_.a_param_,2)*w*swtch;
 	  double LL4 = itilde_(four)/pow(q2,2)*pow(a_param_.a_param_,2)*w*swtch;
 
-	  if (!opts.fixedorder && opts.makecuts)
+	  if (opts.makecuts)
 	    {
 	      phasespace::set_qt(qt);
 	      omegaintegr::genV4p();
@@ -122,4 +186,5 @@ void qtint::calc(double m, double qtmin, double qtmax, int mode)
 	    }
 	}
     }
+
 }
