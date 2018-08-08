af67744404317ddaa193f95ebfb443b93944abf9
Stefano Camarda <stefano.camarda@cern.ch>
2018-08-09 01:05:45 +0200

    CC rules with sin cos weight functions

--- src/clenshawcurtisrules.h
+++ src/clenshawcurtisrules.h
@@ -5,12 +5,15 @@ namespace cc
 {
   extern double xxx[65][65];
   extern double www[65][65];
+  extern double cosw[65][65];
+  extern double sinw[65][65];
 
   extern const double xxx9[9],www9[9];
   extern const double xxx33[33],www33[33];
   extern const double xxx65[65],www65[65];
   
   extern void init();
+  extern void setw(double omega);
 }
 
 #endif
