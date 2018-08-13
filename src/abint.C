4b08d2431b02db9e438d6d2b5aa7b64f2c4633d7
Stefano Camarda <stefano.camarda@cern.ch>
2018-08-13 02:41:30 +0200

    clean up to leave the possibility of removing the change of variable

--- src/abint.C
+++ src/abint.C
@@ -28,17 +28,14 @@ void abint::init()
       double m = 0.5*(b-a);
       for (int j = 0; j < opts.abrule; j++)
 	{
+	  //with change of variable t = eps^(1-x), and with lower cutoff eps on z1 z2
 	  double x = c+m*gr::xxx[opts.abrule-1][j];
 	  double t = min*pow(max/min,x);
 	  double jac=t*lmm;
 	  abx[j+i*opts.abrule]=t;
 	  abw[j+i*opts.abrule]=gr::www[opts.abrule-1][j]*m*jac;
 
-	  ////without change of variable, and without lower cutoff on z1 z2
-	  //double a = 0.+(1.-0.)*i/opts.abintervals;     
-	  //double b = 0.+(1.-0.)*(i+1)/opts.abintervals;
-	  //double c = 0.5*(a+b);
-	  //double m = 0.5*(b-a);
+	  //without change of variable, and without lower cutoff on z1 z2
 	  //abx[j+i*opts.abrule]=c+m*gr::xxx[opts.abrule-1][j];
 	  //abw[j+i*opts.abrule]=gr::www[opts.abrule-1][j]*m;
 	}
