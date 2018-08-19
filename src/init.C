da20b55d729fc047b6b64f1d1ada59dca0cfd5a7
Stefano Camarda <stefano.camarda@cern.ch>
2018-08-19 18:17:58 +0200

    improve readability of init function

--- src/init.C
+++ src/init.C
@@ -52,44 +52,42 @@ void DYTurbo::Init( int argc, char * argv[]){
  * @param _inArg Description of param
  * @return Description of return.
  */
-void DYTurbo::init_params(){
-    // init filling
-    dofill_.doFill_ = 0;
-    dyres::init();
-    mcfm::init();
-    iniflavreduce_(); //need to call this after nproc_.nproc_ is set
-    coupling::initscales();
-    cc::init(); //nodes and weights of Clenshaw-Curtis quadrature rules
-    //cheb::init(); //Chebyshev nodes for Lagrange interpolation
-    //C++ resum
-    //initialise all the C modules
-    gr::init(); //nodes and weights of gaussian quadrature rules
-    mellinint::initgauss(); //gaussian quadrature for mellin inversion
-    mesq::init(); //EW couplings for born amplitudes
-    rapint::init(); //allocate memory for the rapidity quadrature
-    resconst::init(); //calculate beta, A and B coefficients
-    //    if (!opts.fixedorder)
-    //      {
-    anomalous::init(); //calculate anomalous dimensions, C1, C2 and gamma coefficients
-    pdfevol::init(); //transform the PDF from x- to N-space at the factorisation scale
-    if (!opts.fixedorder)
-      {
-	pegasus::init(); //initialise Pegasus QCD and transform the PDF from x- to N-space at the starting scale
-	resint::init(); //initialise dequad integration for the bessel integral
-      }
-    //end C++ resum
-    //V+j fixed order initialisation
-    vjint::init();
-    vjloint::init();
-    //
-    abint::init(); //alfa beta integration initialisation
-    switching::init(); //switching function initialisation
-    itilde::init(); //itilde dequad initialisation
-    rescinit_();
-    // cuba init
-    cubacores(opts.cubacores,1000000);   //< set number of cores (move this to cubainit)
-    cubainit((void (*)()) initfun,NULL); //< merge at the end of the run
-    cubaexit((void (*)()) exitfun,NULL); //< merge at the end of the run
-    // histogram output
+void DYTurbo::init_params()
+{
+  // init filling
+  dofill_.doFill_ = 0;
+  dyres::init();
+  mcfm::init();                          //This functions calls coupling::init(), which in turns call pdfini_() which sets up PDFs from LHAPDF
+  iniflavreduce_();                      //need to call this after nproc_.nproc_ is set
+  coupling::initscales();
+  cc::init();                            //nodes and weights of Clenshaw-Curtis quadrature rules
+  //cheb::init();                        //Chebyshev nodes for Lagrange interpolation
+  //C++ resum: initialise all the C modules
+  gr::init();                            //nodes and weights of gaussian quadrature rules
+  mellinint::initgauss();                //gaussian quadrature for mellin inversion
+  mesq::init();                          //EW couplings for born amplitudes
+  rapint::init();                        //allocate memory for the rapidity quadrature
+  resconst::init();                      //calculate beta, A and B coefficients
+  anomalous::init();                     //calculate anomalous dimensions, C1, C2 and gamma coefficients
+  pdfevol::init();                       //transform the PDF from x- to N-space at the factorisation scale
+  if (!opts.fixedorder)
+    {
+      pegasus::init();                   //initialise Pegasus QCD and transform the PDF from x- to N-space at the starting scale
+      resint::init();                    //initialise dequad integration for the bessel integral
+    }
+  //end C++ resum
+  //V+j fixed order initialisation
+  vjint::init();
+  vjloint::init();
+  //
+  abint::init();                         //alfa beta integration initialisation
+  switching::init();                     //switching function initialisation
+  itilde::init();                        //itilde dequad initialisation
+  rescinit_();
+  // cuba init
+  cubacores(opts.cubacores,1000000);     //< set number of cores (move this to cubainit)
+  cubainit((void (*)()) initfun,NULL);   //< merge at the end of the run
+  cubaexit((void (*)()) exitfun,NULL);   //< merge at the end of the run
+  // histogram output
 }
 
