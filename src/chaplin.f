a03d0c1a36d3ced532c70ab280aaf55354a82cf3
Stefano Camarda <stefano.camarda@cern.ch>
2019-08-05 16:37:13 +0200

    removed chaplin

--- src/chaplin.f
+++ src/chaplin.f
@@ -1,27 +1,38 @@
-c     Interfaces to chaplin harmonic polylogarithmic functions
-      
-c     Dilogarithmic function Li2(x)
       function Li2(x)
       implicit none
-      double precision x,li2
-      double complex z,HPL2
-      external HPL2
+      double precision x,li2,ddilog,fli2
+      external ddilog,fli2
 
-      z = x
-      Li2 = real(hpl2(0,1,z))
+      Li2 = ddilog(x)
+      Li2 = fli2(x)
       
       return
       end
 
-c     Polylogarithmic function Li3(x)
-      function Li3(x)
-      implicit none
-      double precision x,li3
-      double complex z,HPL3
-      external HPL3
-
-      z = x
-      Li3 = real(hpl3(0,0,1,x))
+c     Interfaces to chaplin harmonic polylogarithmic functions
       
-      return
-      end
+!c     Dilogarithmic function Li2(x)
+!      function Li2(x)
+!      implicit none
+!      double precision x,li2
+!      double complex z,HPL2
+!      external HPL2
+!
+!      z = x
+!      Li2 = real(hpl2(0,1,z))
+!      
+!      return
+!      end
+
+!c     Polylogarithmic function Li3(x)
+!      function Li3(x)
+!      implicit none
+!      double precision x,li3
+!      double complex z,HPL3
+!      external HPL3
+!
+!      z = x
+!      Li3 = real(hpl3(0,0,1,x))
+!      
+!      return
+!      end
