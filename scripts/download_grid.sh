f2d8933e793247847542a41a279c27900bcce479
Stefano Camarda <stefano.camarda@cern.ch>
2019-08-08 15:34:31 +0200

    download with rucio

--- scripts/download_grid.sh
+++ scripts/download_grid.sh
@@ -2,7 +2,7 @@
 
 if [[ -z $1 ]]
 then
-    "Usage: $0 <jobID>"
+    echo "usage: $0 <jobID>"
     exit
 fi
 
@@ -10,4 +10,22 @@ jid=$1
 
 mkdir -p out
 cd out
-rucio download user.*.${jid}.*.results.root download user.*.${jid}.*.results.txt user.*.*.${jid}.*.log.tgz
+rucio download user.${USER}.${jid}.*.results.root download user.${USER}.${jid}.'*'.results.txt user.${USER}.'*'.${jid}.*.log.tgz
+
+tag=`cd user.${USER}; ls *.tgz |cut -d . -f3 |uniq`
+mkdir -p $tag/root
+mkdir -p $tag/txt
+mkdir -p $tag/log
+mv user.${USER}/*.root $tag/root
+mv user.${USER}/*.txt $tag/txt
+mv user.${USER}/*.tgz $tag/log
+
+rmdir user.${USER}
+
+cd $tag/log
+
+for file in `ls *.tgz`
+do
+    tar -xzf $file
+    rm $file
+done
