372ec22bfce6c3ca05528f924f234e3dd93005e0
Stefano Camarda <stefano.camarda@cern.ch>
2019-10-04 17:35:25 +0200

    improved download script

--- scripts/download_grid.sh
+++ scripts/download_grid.sh
@@ -10,15 +10,18 @@ jid=$1
 
 mkdir -p out
 cd out
-rucio download user.${USER}.${jid}.*.results.root download user.${USER}.${jid}.'*'.results.txt user.${USER}.'*'.${jid}.*.log.tgz
+rucio download user.${USER}.${jid}.*.results.root 
+rucio download user.${USER}.${jid}.'*'.results.txt 
+rucio download user.${USER}.'*'.${jid}.*.log.tgz
 
 tag=`cd user.${USER}; ls *.tgz |cut -d . -f3 |uniq`
+
 mkdir -p $tag/root
 mkdir -p $tag/txt
 mkdir -p $tag/log
-mv user.${USER}/*.root $tag/root
-mv user.${USER}/*.txt $tag/txt
-mv user.${USER}/*.tgz $tag/log
+mv user.${USER}/*${jid}*.root $tag/root
+mv user.${USER}/*${jid}*.txt $tag/txt
+mv user.${USER}/*${jid}*.tgz $tag/log
 
 rmdir user.${USER}
 
