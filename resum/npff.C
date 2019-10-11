072214112ea6f3435cc6946a9e2ca1c5a0cade7f
Stefano Camarda <stefano.camarda@cern.ch>
2019-10-12 00:35:46 +0200

    NP parameterisations

--- resum/npff.C
+++ resum/npff.C
@@ -3,7 +3,8 @@
 #include "resconst.h"
 #include "pdfevol.h"
 #include "resint.h"
-#include <LHAPDF/LHAPDF.h>
+//#include <LHAPDF/LHAPDF.h>
+#include "pdf.h"
 #include <iostream>
 
 complex <double> npff::uvff;
@@ -17,71 +18,44 @@ complex <double> npff::glff;
 
 complex <double> npff::S(complex <double> b, double m, double x1, double x2)
 {
-  complex <double> ff;
+  complex <double> ff = 1;
     
-  double a;
-  double g1, g2, g3;
-  double Q0;
-  
-  //Standard dyres 1-parameter
-  a = opts.g_param;
-
+  //Gaussian form
   //BLNY (blim = 0.5)
-  //g1 = 0.21;
-  //g2 = 0.68;
-  //g3 = -0.13;
-  //Q0 = 3.2;
-
+  //g1 = 0.21;  //g2 = 0.68;  //g3 = -0.13;  //Q0 = 3.2;
   //Konychev Nadolsky (blim = 1.5)
-  //g1 = 0.20;
-  //g2 = 0.19;
-  //g3 = -0.03;
-  //Q0 = 3.2;  
-
+  //g1 = 0.20;  //g2 = 0.19;  //g3 = -0.03;  //Q0 = 3.2;  
   //M. Hirai, H. Kawamura, K. Tanaka, https://inspirehep.net/record/1229722 (minimal prescription, MSTW08)
-  //g1 = 0.330;
-  //g2 = 0.066;
-  //g3 = 0.;
-  //Q0 = 2.*1.3;
-  
-  //g1 = 0.50;
-  //g2 = 2.0;
-  //g3 = 0.;
-  //Q0 = 50;
+  //g1 = 0.330;  //g2 = 0.066;  //g3 = 0.;  //Q0 = 2.*1.3;
+  //g1 = 0.50;   //g2 = 2.0;  //g3 = 0.;  //Q0 = 50;
+  if (opts.npff == 0)
+    ff = exp(-(opts.g1 + opts.g2 * log(m/opts.Q0) + opts.g3*log(100*x1*x2)) * pow(b,2));
 
-  
-  //a = g1 + g2 * log(m/Q0) + g3*log(100*x1*x2);
-
-  
-  //cout << "m " << m << " g " << a << endl;
-  //cout << a1 << "  " << a2 * log(m/Q0) << "  " << a3*log(100*x1*x2) << endl;
-
-  //gaussian form
-  ff = exp(-a*pow(b,2));
-
-  //exponential form
-  //ff = exp(-a*b);
-
+  //Exponential form
+  if (opts.npff == 1)
+    ff = exp(-opts.e*b);
 
   //Collins, Rogers, large bT form (https://arxiv.org/abs/1412.3820)
-  //double g0 = 0.3;
-  //double gk = g0 * (1. - exp(- (resconst::Cf*LHAPDF::alphasPDF(real(pdfevol::qbstar))*pow(real(b),2))/(M_PI*g0*pow(blimit_.rblim_,2))));
-  //Q0 = 1.6;
-  //ff = exp(-gk*log(pow(m/Q0,2)));
-
-  //flavor dependent form factors
-  if (opts.flavour_kt)
+  //g0 = 0.3; //Q0 = 1.6;
+  if (opts.npff == 2)
     {
-      uvff = exp(-a/2*pow(b,2));
-      usff = exp(-a/2*pow(b,2));
-      dvff = exp(-a/2*pow(b,2));
-      dsff = exp(-a/2*pow(b,2));
-      ssff = exp(-a/2*pow(b,2));
-      chff = exp(-a/2*pow(b,2));
-      boff = exp(-a/2*pow(b,2));
-      glff = exp(-a/2*pow(b,2));
+      double gk = opts.g0 * (1. - exp(- (resconst::Cf*pdf::alphas(real(pdfevol::qbstar))*pow(real(b),2))/(M_PI*opts.g0*pow(blimit_.rblim_,2))));
+      ff = exp(-gk*log(pow(m/opts.Q0,2)));
     }
 
+  //Additional flavor dependent form factors
+  if (opts.flavour_kt)
+    {
+      uvff = exp(-opts.g1_uv*pow(b,2)) * ff;
+      usff = exp(-opts.g1_us*pow(b,2)) * ff;
+      dvff = exp(-opts.g1_dv*pow(b,2)) * ff;
+      dsff = exp(-opts.g1_ds*pow(b,2)) * ff;
+      ssff = exp(-opts.g1_ss*pow(b,2)) * ff;
+      chff = exp(-opts.g1_ch*pow(b,2)) * ff;
+      boff = exp(-opts.g1_bo*pow(b,2)) * ff;
+      glff = exp(-opts.g1_gl*pow(b,2)) * ff;
+    }
+  
   //universal form factor
   return ff;
 }
