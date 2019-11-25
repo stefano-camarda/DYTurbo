3e93ab500420fec46f140daf241f0ebcacf91200
Stefano Camarda <stefano.camarda@cern.ch>
2019-11-25 11:12:06 +0100

    added A4q and B3q coefficients

--- resum/resconst.C
+++ resum/resconst.C
@@ -1,5 +1,8 @@
 #include "resconst.h"
 #include <math.h>
+#include <iostream>
+
+using namespace std;
 
 // constants.f
 const int resconst::NF = 5;
@@ -19,12 +22,14 @@ const double resconst::Cf = 4./3.;
 //even higher precision
 const double resconst::Euler = 0.57721566490153286;
 const double resconst::Z2 = M_PI*M_PI/6.;
-const double resconst::Z3 = 1.2020569031595942853;
+const double resconst::Z3 = 1.20205690315959428540;
+const double resconst::Z4 = 1.08232323371113819152;
+const double resconst::Z5 = 1.03692775514336992633;
 
 double resconst::b0;
-double resconst::beta0, resconst::beta1, resconst::beta2, resconst::Kappa;
+double resconst::beta0, resconst::beta1, resconst::beta2, resconst::beta3, resconst::beta4, resconst::Kappa;
 double resconst::A1g, resconst::A2g, resconst::A3g, resconst::B1g, resconst::B2g, resconst::C1ggn;
-double resconst::A1q, resconst::A2q, resconst::A3q, resconst::B1q, resconst::B2q, resconst::C1qqn;
+double resconst::A1q, resconst::A2q, resconst::A3q, resconst:: A4q, resconst::B1q, resconst::B2q, resconst::C1qqn, resconst::B3q;
 double resconst::C1qqdelta, resconst::Delta2qq;
 double resconst::D0qqqq, resconst::D1qqqq;
 double resconst::Deltaqqqq;
@@ -34,8 +39,10 @@ double resconst::H2qqD0;
 void resconst::init()
 {
   double pi2 = M_PI*M_PI;
+  double pi3 = pi2*M_PI;
   double pi4 = pi2*pi2;
   double nf2 = NF*NF;
+  double nf3 = nf2*NF;
 
   b0=2*exp(-Euler);
 
@@ -43,6 +50,12 @@ void resconst::init()
   beta0=(33.-2.*NF)/12.;
   beta1=(153.-19.*NF)/24.;
   beta2=2857./128.-5033.*NF/1152.+325.*nf2/3456.;
+  beta3=(149753./6. + 3564.*Z3 + NF*(-1078361./162.-6508./27.*Z3)+nf2*(50065./162.+6472./81.*Z3)+1093./729.*nf3)/256.; //from https://arxiv.org/pdf/1701.01404.pdf Eq.(3.6), with a 4^4=256 normalisation factor
+  beta4=(8157455./16. + 621885./2.*Z3 - 88209./2.*Z4 - 288090.*Z5
+    +NF* (-336460813./1944. - 4811164./81.*Z3 + 33935./6.*Z4 + 1358995./27.*Z5)
+    +NF*NF*(25960913./1944. + 698531./81.*Z3 - 10526./9.*Z4 - 381760./81.*Z5)
+    *NF*NF*NF*(-630559./5832. - 48722./243.*Z3 + 1618./27.*Z4 + 460./9.*Z5)
+	 +NF*NF*NF*NF*(1205./2916. - 152./81.*Z3))/1024.;  //from https://arxiv.org/pdf/1701.01404.pdf Eq.(3.7), with a 4^5=1024 normalisation factor
   Kappa=67./6.-pi2/2.-5./9.*NF;
 
   //gluon coefficients
@@ -56,12 +69,53 @@ void resconst::init()
   //quark coefficients
   A1q=Cf; //4./3;
   A2q=Cf/2.*Kappa; //(67./6.-pi2/2.-5./9.*NF);
-  A3q=Cf*(13.81-2.15*NF-nf2/108.)+Cf*(CA*(29.9259-28.*Z3)-8.2963*NF/2.)*2.*(beta0*4.)/64.; //A3 from Becher & Neubert
-  B1q=-(3.*Cf)/2.; //-2;
-  B2q=Cf*Cf*(pi2/4.-3./16.-3.*Z3)+CA*Cf*(11.*pi2/36.-193./48.+3.*Z3/2.)+Cf*NF*(17./24.-pi2/18.);
-  //B2q=4./9.*(pi2-3./4.-12.*Z3)+(11./9.*pi2-193./12.+6.*Z3)+NF/6.*(17./3.-4./9.*pi2);
+  //A3q=Cf*(13.81-2.15*NF-nf2/108.)+Cf*(CA*(29.9259-28.*Z3)-8.2963*NF/2.)*2.*(beta0*4.)/64.; //A3 from Becher & Neubert
+  //cout << "A3q orig " << A3q << endl;
+  //Better approximation for A3q:
+  //A3q=Cf*(13.768339123-2.15*NF-nf2/108.)+Cf*(CA*(29.9259-28.*Z3)-8.2963*NF/2.)*2.*(beta0*4.)/64.;
+  A3q=Cf*( (2*CA*CA*(245./24.-67./9.*Z2+11./6.*Z3+11./5.*Z2*Z2)/8.)
+	  +(2*(Cf*(-55./24.+2*Z3)+CA*(-209./108.+10./9.*Z2-7./3.*Z3))/8.)*NF-nf2/108.)
+    +Cf*(CA*((808./27.)-28.*Z3)-(224./54.*2.)*NF/2.)*2.*(beta0*4.)/64.; //A3 from Becher & Neubert (https://arxiv.org/pdf/1007.4005.pdf Eq.(74))
+  //cout << "A3q fix " << A3q << endl;
+
+  //A4q from https://arxiv.org/pdf/1705.09127.pdf
+  //A4q=(CA*CA*CA*Cf*(121./3.*Z3*Z2 - 8789./162.*Z2 - 19093./54.*Z3 - 847./24.*Z4 + 132.*Z5 + 3761815./11664.)
+  //     + CA*CA*Cf*NF*(-22./3.*Z3*Z2 + 2731./162.*Z2 + 4955./54.*Z3 + 11./6.*Z4 -24*Z5 - 31186./243.)
+  //     + CA*Cf*Cf*NF*(272./9.*Z3 + 11.*Z4 - 7351./144.)
+  //     + CA*Cf*nf2*(-103./81.*Z2 - 47./27.*Z3 +5./6.*Z4 + 13819./972.)
+  //     + Cf*Cf*nf2*(-38./9.*Z3 - 2*Z4 + 215./24.)
+  //     + Cf*NF*NF*NF*(-4./9.*Z3-232./729.))/16.;
+  //A4q=(-1189.19)/16.; //simplied formula for NF=5
+    
+  
+  //from https://arxiv.org/pdf/1604.01404.pdf
+  double gamma1r = Cf*CA*(28*Z3-808./27.) + 112.*Cf*NF/27.;
+  double gamma2r = Cf*CA*CA*(-176./3*Z3*Z2 + 6392./81.*Z2 + 12328./27.*Z3 +154./3.*Z4 - 192.*Z5 - 297029./729.)
+    + Cf*CA*NF* ( -824./81.*Z2 - 904./27.*Z3 +20./3.*Z4 + 62626./729.)
+    + Cf*nf2* ( -32./9.*Z3 - 1856./729.)
+    + Cf*Cf*NF*(-304./9.*Z3-16.*Z4 + 1711./27.);
+
+  double D2q = -gamma1r;
+  double D3q = -gamma2r;
+
+  //double A4q_th = 1553./256. //Pade approximant from https://arxiv.org/pdf/hep-ph/0506288.pdf
+  double A4q_th = 0.42441*0.0133*pow(M_PI,4); //from Eq. (4.3) of https://arxiv.org/pdf/1805.09638.pdf
+  
+  A4q = A4q_th
+    + (3*beta0*4*D3q + 2*beta1*16*D2q)/256.; //formula from Eq. (71) of https://arxiv.org/pdf/1007.4005.pdf with 4^4 normalisation.
+
+  B1q=-(3.*Cf)/2.;                                                                                //-2;
+  B2q=Cf*Cf*(pi2/4.-3./16.-3.*Z3)+CA*Cf*(11.*pi2/36.-193./48.+3.*Z3/2.)+Cf*NF*(17./24.-pi2/18.);  //B2q=4./9.*(pi2-3./4.-12.*Z3)+(11./9.*pi2-193./12.+6.*Z3)+NF/6.*(17./3.-4./9.*pi2);
+  //B3q=-116.685/8. // from https://arxiv.org/pdf/1705.09127.pdf, but does not look OK
+  B3q=(7358.86 -721.516*NF + 20.5951* nf2)/64.; // from https://arxiv.org/pdf/1604.01404.pdf
+
   C1qqn=Cf/2.*(pi2/2.-4.);          // Only delta(1-z) part, i.e. N independent part
 
+//  // Only delta(1-z) part, i.e. N independent part
+//  C2qqn = 1./2.* (CA*Cf*(59.*Z3/18. - 1535./192. + 215.*pi2/216. - pi4/240.)
+//		  + Cf*NF*(192.*Z3 + 1143. - 152.*pi2)/864.
+//		  + Cf*Cf*(-15.*Z3 + 511./16. - 67.*pi2/12. + 17.*pi4/45.)/4. - Cf*Cf* pow(pi2-8.,2)/16.);
+  
   // Delta term in c1qq coefficient
   C1qqdelta=(pi2-8.)/3.;
 
