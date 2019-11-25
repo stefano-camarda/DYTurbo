22cd41e6bc0668e8a106a520facba53b579c4f26
Stefano Camarda <stefano.camarda@cern.ch>
2019-11-26 00:17:18 +0100

    NNNLL for aexp

--- resum/sudakov.f
+++ resum/sudakov.f
@@ -53,7 +53,7 @@ c     print *,aass*pi,dyalphas_lhapdf(q),dyalphas_lhapdf(q/a_param),sqrt(nq2),xl
 !c     HERE now a dependence (without constant term)!
 !      log1xlambda=log(1-xlambda)
 !      aa1=log1xlambda+aass*xlp*
-!     .      (beta1/beta0*log1xlambda/(1-xlambda) 
+!     .      (beta1/beta0*log1xlambda/(1-xlambda)
 !     .       + beta0*xlambda/(1-xlambda)*rlogq2mur2
 !c     .       +   beta0*log(q2/muf2)
 !     .       -2d0*beta0*xlambda*rloga/(1-xlambda)   )
@@ -62,13 +62,14 @@ c     print *,aass*pi,dyalphas_lhapdf(q),dyalphas_lhapdf(q/a_param),sqrt(nq2),xl
       aa1=log1xlambda
       if (iord.ge.1) then
          aa1 = aa1+aass*
-     .        beta1/beta0*log1xlambda/(1-xlambda) 
+     .        beta1/beta0*log1xlambda/(1-xlambda)
       endif
       if (iord.ge.2) then
          aa1 = aa1+aass**2*
      .       ((beta1**2/beta0**2-beta2/beta0)*xlambda/(1d0-xlambda)**2
      .       +(beta1**2/beta0**2)*log1xlambda/(1d0-xlambda)**2
-     .       +(beta1**2/beta0**2)*log1xlambda**2/(2d0*(1d0-xlambda)**2))
+c     .       +(beta1**2/beta0**2)*log1xlambda**2/(2d0*(1d0-xlambda)**2))
+     .       -(beta1**2/beta0**2)*log1xlambda**2/(2d0*(1d0-xlambda)**2))
       endif
 
 !     Scale dependence
@@ -78,7 +79,9 @@ c     print *,aass*pi,dyalphas_lhapdf(q),dyalphas_lhapdf(q/a_param),sqrt(nq2),xl
       endif
       if (iord.ge.2) then
          aa1 = aa1+aass**2*(rlogq2mur2-2d0*rloga)*
-     .     beta1*(xlambda/(1d0-xlambda)**2+log1xlambda/(1d0-xlambda)**2)
+c     .     beta1*(xlambda/(1d0-xlambda)**2+log1xlambda/(1d0-xlambda)**2)
+     .        (beta1*(xlambda/(1d0-xlambda)**2
+     .        -beta1*log1xlambda/(1d0-xlambda)**2))
       endif
       
       alphasl=Exp(-aa1)
@@ -92,16 +95,6 @@ c.....Now compute the factors
 c.....needed to resum the logs which multiply the N-dependent part
 c.....of the C coefficients
 
-!      aa2= xlambda/(1- xlambda)
-!      aexpB=Exp(aa2) 
-!
-!      aa3 = xlambda*(xlambda-2d0)/(1-xlambda)**2 ! --> should be xlambda*(xlambda-2d0)/(2*(1-xlambda)**2) ?
-!      aexpC=Exp(aa3) 
-!
-!      aa4 = log1xlambda/(1-xlambda)**2 ! --> should be (xlambda*(2d0-xlambda)+2d0*log1xlambda)/(2d0*(1-xlambda)**2) ?
-!      aexpD=Exp(aa4) 
-
-      
 c  the limit below implies xlambda<1/2 and then aa2<= 1      
 c      blim=b0p*(1/q)*exp(1/(2*as*beta0)) 
 c      blim=b0p*(1/q)*exp(1/(4*as*beta0))
@@ -111,6 +104,7 @@ C Set a limit to avoid very large values of b (= very small scales ~1/b)
 !     without this additional blim some scale variations will fail (when mures > muren)
       blim=0.5d0 ! --> allow this to a separate blim in the settings, or set it using muren instead of mures
 !      blim=1.1229190d0
+!      blim=3d0
 
       if (flagrealcomplex.eq.0) bstar=b/sqrt(1+(b**2)/(blim**2))
       if (imod.eq.1) blog=log( (q*bstar/b0p)**2 + 1) !modified sudakov
@@ -119,23 +113,47 @@ C Set a limit to avoid very large values of b (= very small scales ~1/b)
       xlambda=beta0*aass*blog
 
 c     HERE now a dependence (without constant term)!
+c      log1xlambda=log(1-xlambda)
+c      aa1=log1xlambda+aass*xlp*
+c     .      (beta1/beta0*log1xlambda/(1-xlambda)
+c     .       + beta0*xlambda/(1-xlambda)*rlogq2mur2
+c     .       -2d0*beta0*xlambda*rloga/(1-xlambda)   )
+
       log1xlambda=log(1-xlambda)
-      aa1=log1xlambda+aass*xlp*
-     .      (beta1/beta0*log1xlambda/(1-xlambda) 
-     .       + beta0*xlambda/(1-xlambda)*rlogq2mur2
-     .       -2d0*beta0*xlambda*rloga/(1-xlambda)   )
+      aa1=log1xlambda
+      if (iord.ge.1) then
+         aa1 = aa1+aass*
+     .        beta1/beta0*log1xlambda/(1-xlambda)
+      endif
+      if (iord.ge.2) then
+         aa1 = aa1+aass**2*
+     .       ((beta1**2/beta0**2-beta2/beta0)*xlambda/(1d0-xlambda)**2
+     .       +(beta1**2/beta0**2)*log1xlambda/(1d0-xlambda)**2
+     .       -(beta1**2/beta0**2)*log1xlambda**2/(2d0*(1d0-xlambda)**2))
+      endif
+
+!     Scale dependence
+      if (iord.ge.1) then
+         aa1 = aa1+aass*(rlogq2mur2-2d0*rloga)*
+     .        beta0*xlambda/(1-xlambda)
+      endif
+      if (iord.ge.2) then
+         aa1 = aa1+aass**2*(rlogq2mur2-2d0*rloga)*
+     .        (beta1*(xlambda/(1d0-xlambda)**2
+     .        -beta1*log1xlambda/(1d0-xlambda)**2))
+      endif
+      
       aexp=Exp(-aa1)      
 
-      aa2= xlambda/(1- xlambda)
+      aa2= xlambda/(1d0-xlambda)
       aexpB=Exp(aa2) 
 
-      aa3 = xlambda*(xlambda-2d0)/(1-xlambda)**2 ! --> should be xlambda*(xlambda-2d0)/(2*(1-xlambda)**2) ?
+      aa3 = xlambda*(xlambda-2d0)/(1d0-xlambda)**2
       aexpC=Exp(aa3) 
 
-      aa4 = log1xlambda/(1-xlambda)**2 ! --> should be (xlambda*(2d0-xlambda)+2d0*log1xlambda)/(2d0*(1-xlambda)**2) ?
+      aa4 = log1xlambda/(1d0-xlambda)**2
       aexpD=Exp(aa4) 
 
-      
       return
       end
       
