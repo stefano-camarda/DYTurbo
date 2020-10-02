1bf6dc310ed4abe9707ccc3649a9a5cab6cb74b7
Stefano Camarda <stefano.camarda@cern.ch>
2020-10-02 11:48:14 +0200

    Improved approximation for the Mellin transform of: Li2(x)/(1+x)

--- pegasus/pns1mom.f
+++ pegasus/pns1mom.f
@@ -57,8 +57,8 @@
        S1 = S(KN,1)
        S2 = S(KN,2)
 *
-       N1 = N + 1.
-       N2 = N + 2.
+       N1 = N + 1D0
+       N2 = N + 2D0
        NS = N * N
        NT = NS * N
        NFO = NT * N
@@ -70,32 +70,33 @@
 * ..Analytic continuations of the occuring sums as given in GRV (1990) 
 *   (with an improved parametrization of the moments of  Sp(x)/(1+x).)
 *
-       N3 = N + 3.
-       N4 = N + 4.
-       N5 = N + 5.
-       N6 = N + 6.
-       S11 = S1  + 1./N1
-       S12 = S11 + 1./N2
-       S13 = S12 + 1./N3
-       S14 = S13 + 1./N4
-       S15 = S14 + 1./N5
-       S16 = S15 + 1./N6
-       SPMOM = 1.0000D0 * (ZETA(2) - S1 / N ) / N  -
-     1         0.9992D0 * (ZETA(2) - S11/ N1) / N1 +
-     2         0.9851D0 * (ZETA(2) - S12/ N2) / N2 -
-     3         0.9005D0 * (ZETA(2) - S13/ N3) / N3 +
-     4         0.6621D0 * (ZETA(2) - S14/ N4) / N4 -
-     5         0.3174D0 * (ZETA(2) - S15/ N5) / N5 +
-     6         0.0699D0 * (ZETA(2) - S16/ N6) / N6  
+!       N3 = N + 3D0
+!       N4 = N + 4D0
+!       N5 = N + 5D0
+!       N6 = N + 6D0
+!       S11 = S1  + 1D0/N1
+!       S12 = S11 + 1D0/N2
+!       S13 = S12 + 1D0/N3
+!       S14 = S13 + 1D0/N4
+!       S15 = S14 + 1D0/N5
+!       S16 = S15 + 1D0/N6
+!       SPMOM = 1.0000D0 * (ZETA(2) - S1 / N ) / N  -
+!     1         0.9992D0 * (ZETA(2) - S11/ N1) / N1 +
+!     2         0.9851D0 * (ZETA(2) - S12/ N2) / N2 -
+!     3         0.9005D0 * (ZETA(2) - S13/ N3) / N3 +
+!     4         0.6621D0 * (ZETA(2) - S14/ N4) / N4 -
+!     5         0.3174D0 * (ZETA(2) - S15/ N5) / N5 +
+!     6         0.0699D0 * (ZETA(2) - S16/ N6) / N6  
+       SPMOM=ACG3(N-1D0) ! --> Improved approximation for the Mellin transform of: Li2(x)/(1+x)
 *
-       SLC = - 5./8.D0 * ZETA(3)
-       SLV = - ZETA(2)/2.* (PSI(N1/2.) - PSI(N/2.)) + S1/NS + SPMOM
+       SLC = - 5D0/8D0 * ZETA(3)
+       SLV = - ZETA(2)/2D0* (PSI(N1/2D0) - PSI(N/2D0)) + S1/NS + SPMOM
        SSCHLM = SLC - SLV
-       SSTR2M = ZETA(2) - DPSI (N1/2.,1)
-       SSTR3M = 0.5 * DPSI (N1/2.,2) + ZETA(3)
+       SSTR2M = ZETA(2) - DPSI (N1/2D0,1)
+       SSTR3M = 0.5D0 * DPSI (N1/2D0,2) + ZETA(3)
        SSCHLP(KN) = SLC + SLV
-       SSTR2P(KN) = ZETA(2) - DPSI (N2/2.,1)
-       SSTR3P(KN) = 0.5 * DPSI (N2/2.,2) + ZETA(3)
+       SSTR2P(KN) = ZETA(2) - DPSI (N2/2D0,1)
+       SSTR3P(KN) = 0.5D0 * DPSI (N2/2D0,2) + ZETA(3)
 *
 * ---------------------------------------------------------------------
 *
@@ -104,22 +105,22 @@
 *    from these moments of the splitting functions by factors -1 or -2,
 *    in addition to possible different normalizations of the coupling)
 *
-       PNMA = ( 16.* S1 * (2.* N + 1.) / (NS * N1S) +
-     1          16.* (2.* S1 - 1./(N * N1)) * ( S2 - SSTR2M ) +
-     2          64.* SSCHLM + 24.* S2 - 3. - 8.* SSTR3M -
-     3          8.* (3.* NT + NS -1.) / (NT * N1T) +
-     4          16.* (2.* NS + 2.* N +1.) / (NT * N1T) ) * (-0.5)
-       PNPA = ( 16.* S1 * (2.* N + 1.) / (NS * N1S) +
-     1          16.* (2.* S1 - 1./(N * N1)) * ( S2 - SSTR2P(KN) ) +
-     2          64.* SSCHLP(KN) + 24.* S2 - 3. - 8.* SSTR3P(KN) -
-     3          8.* (3.* NT + NS -1.) / (NT * N1T) -
-     4          16.* (2.* NS + 2.* N +1.) / (NT * N1T) ) * (-0.5)
-       PNSB = ( S1 * (536./9. + 8.* (2.* N + 1.) / (NS * N1S)) -
-     1          (16.* S1 + 52./3.- 8./(N * N1)) * S2 - 43./6. -
-     2          (151.* NFO + 263.* NT + 97.* NS + 3.* N + 9.) *
-     3          4./ (9.* NT * N1T) ) * (-0.5)
-       PNSC = ( -160./9.* S1 + 32./3.* S2 + 4./3. +
-     1          16.* (11.* NS + 5.* N - 3.) / (9.* NS * N1S) ) * (-0.5)
+       PNMA = ( 16D0* S1 * (2D0* N + 1D0) / (NS * N1S) +
+     1          16D0* (2D0* S1 - 1D0/(N * N1)) * ( S2 - SSTR2M ) +
+     2          64D0* SSCHLM + 24D0* S2 - 3D0 - 8D0* SSTR3M -
+     3          8D0* (3D0* NT + NS -1D0) / (NT * N1T) +
+     4          16D0* (2D0* NS + 2D0* N +1D0) / (NT * N1T) ) * (-0.5D0)
+       PNPA = ( 16D0* S1 * (2D0* N + 1D0) / (NS * N1S) +
+     1          16D0* (2D0* S1 - 1D0/(N * N1)) * ( S2 - SSTR2P(KN) ) +
+     2          64D0* SSCHLP(KN) + 24D0* S2 - 3D0 - 8D0* SSTR3P(KN) -
+     3          8D0* (3D0* NT + NS -1D0) / (NT * N1T) -
+     4          16D0* (2D0* NS + 2D0* N +1D0) / (NT * N1T) ) * (-0.5D0)
+       PNSB = ( S1 * (536D0/9D0 + 8D0* (2D0* N + 1D0) / (NS * N1S)) -
+     1          (16D0* S1 + 52D0/3D0- 8D0/(N * N1)) * S2 - 43D0/6D0 -
+     2          (151D0* NFO + 263D0* NT + 97D0* NS + 3D0* N + 9D0) *
+     3          4D0/ (9D0* NT * N1T) ) * (-0.5D0)
+       PNSC = ( -160D0/9D0* S1 + 32D0/3D0* S2 + 4D0/3D0 +
+     1      16D0* (11D0* NS + 5D0* N - 3D0) / (9D0* NS * N1S) )*(-0.5D0)
 *
 * ---------------------------------------------------------------------
 *
@@ -127,8 +128,8 @@
 *
        DO 2 NF = NFMIN, NFMAX
 *
-       P1NS(KN,NF,1) = CF * ((CF-CA/2.)* PNPA + CA* PNSB + TR*NF* PNSC)
-       P1NS(KN,NF,2) = CF * ((CF-CA/2.)* PNMA + CA* PNSB + TR*NF* PNSC)
+       P1NS(KN,NF,1) = CF * ((CF-CA/2D0)* PNPA + CA* PNSB + TR*NF* PNSC)
+       P1NS(KN,NF,2) = CF * ((CF-CA/2D0)* PNMA + CA* PNSB + TR*NF* PNSC)
        P1NS(KN,NF,3) = P1NS(KN,NF,2)
 *
 * ---------------------------------------------------------------------
