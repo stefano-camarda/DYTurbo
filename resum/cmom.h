714ac9f1288f35636021867879cb7831a762347b
Stefano Camarda <stefano.camarda@cern.ch>
2021-01-04 15:53:04 +0100

    analytic continuation of C coefficients

---
+++ resum/cmom.h
@@ -0,0 +1,21 @@
+#ifndef cmom_h
+#define cmom_h
+
+#include <complex>
+using namespace std;
+
+extern complex <double> C1qgN(complex <double> N);
+extern complex <double> C1qqN(complex <double> N);
+
+extern complex <double> C2qgN(complex <double> N);
+extern complex <double> C2qqN(complex <double> N);
+extern complex <double> C2qqpN(complex <double> N);
+extern complex <double> C2qqbN(complex <double> N);
+
+extern complex <double> C3qgN(complex <double> N);
+extern complex <double> C3qqN(complex <double> N);
+extern complex <double> C3qqpN(complex <double> N);
+extern complex <double> C3qqbN(complex <double> N);
+extern complex <double> C3qqbpN(complex <double> N);
+
+#endif
