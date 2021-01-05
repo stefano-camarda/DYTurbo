fd3cac3843c2a0b7cecbd8427a978dad20fe54a2
Stefano Camarda <stefano.camarda@cern.ch>
2021-01-05 11:47:22 +0100

    LTS variables for multithreding

--- pegasus/psg2mom.f
+++ pegasus/psg2mom.f
@@ -23,26 +23,27 @@
        SUBROUTINE PSG2MOM 
 *
        IMPLICIT DOUBLE COMPLEX (A - Z)
-       INTEGER NMAX, NDIM, NFMIN, NFMAX, IAPP2, KN, NF
-       PARAMETER (NDIM = 512, NFMIN = 3, NFMAX = 6)
-       DOUBLE PRECISION ZETA(6)
-*
-* ---------------------------------------------------------------------
-*
-* ..Input common-blocks 
-*
-       COMMON / MOMS   / NA (NDIM)
-       COMMON / NNUSED / NMAX
-       COMMON / HSUMS  / S(NDIM,6)
-       COMMON / RZETA  / ZETA
-*
-       COMMON / PNS2   / P2NS (NDIM, NFMIN:NFMAX, 3)
-*
-* ..Output common-block 
-*
-       COMMON / PSG2   / P2SG (NDIM, NFMIN:NFMAX, 2, 2)
-*
-* ---------------------------------------------------------------------
+!       INTEGER NMAX, NDIM, NFMIN, NFMAX, IAPP2, KN, NF
+!       include 'dimensions.f'
+!       DOUBLE PRECISION ZETA(6)
+!*
+!* ---------------------------------------------------------------------
+!*
+!* ..Input common-blocks 
+!*
+!       COMMON / MOMS   / NA (NDIM)
+!       COMMON / NNUSED / NMAX
+!       COMMON / HSUMS  / S(NDIM,6)
+!       COMMON / RZETA  / ZETA
+!*
+!       COMMON / PNS2   / P2NS (NDIM, NFMIN:NFMAX, 3)
+!*
+!* ..Output common-block 
+!*
+!       COMMON / PSG2   / P2SG (NDIM, NFMIN:NFMAX, 2, 2)
+!*
+!* ---------------------------------------------------------------------
+      include 'pnsg_inc.f'
 *
 * ..Begin of the Mellin-N loop
 *
