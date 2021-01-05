7d7d5a367770c2547651a0797d40f3a9c245b14e
Stefano Camarda <stefano.camarda@cern.ch>
2021-01-05 14:08:12 +0100

    b line

--- test/points.C
+++ test/points.C
@@ -18,6 +18,7 @@
 #include "loint.h"
 #include "scales.h"
 #include "sudakovff.h"
+#include "besselint.h"
 
 #include <iostream>
 #include <iomanip>
@@ -48,8 +49,8 @@ int main( int argc , char * argv[])
   //Initialization
   ///@todo: print out EW parameters and other settings
   // just a check
-  opts.dumpAll();
-  DYTurbo::PrintTable::Settings();
+  if (!opts.silent) opts.dumpAll();
+  if (!opts.silent) DYTurbo::PrintTable::Settings();
   /***********************************/
 
   double costh, m, qt, y;
@@ -74,40 +75,44 @@ int main( int argc , char * argv[])
   
   /**************************************/
   //Checks for resummed cross section
-  cout << "To match the numbers between fortran and C++ set:" << endl;
-  cout << "mellinrule = 64     #number of nodes" << endl;
-  cout << "zmax = 27.          #upper" << endl;
-  cout << "cpoint = 1          " << endl;
-  cout << "mellin1d = false          " << endl;
-  cout << "bintaccuracy = 1.0e-2  #accuracy" << endl;
-  cout << endl;
+  if (!opts.silent)
+    {
+      cout << "To match the numbers between fortran and C++ set:" << endl;
+      cout << "mellinrule = 64     #number of nodes" << endl;
+      cout << "zmax = 27.          #upper" << endl;
+      cout << "cpoint = 1          " << endl;
+      cout << "mellin1d = false          " << endl;
+      cout << "bintaccuracy = 1.0e-2  #accuracy" << endl;
+      cout << endl;
+    }
   //  std::cout << std::setprecision(15);
 
-  costh = 0.; m = opts.rmass; qt = 1; y = 0;
-  test_resum_speed(costh,m,qt,y,mode);
+  //  costh = 0.; m = opts.rmass; qt = 1; y = 0;
+  //  test_resum_speed(costh,m,qt,y,mode);
 
-  costh = 0.1; m = 91; qt = 5; y = 0.2;
-  test_resum_speed(costh,m,qt,y,mode);
+//  costh = 0.1; m = 91; qt = 5; y = 0.2;
+//  test_resum_speed(costh,m,qt,y,mode);
+//
+//  costh = 0.5; m = 70; qt = 10; y = 1.5;
+//  test_resum_speed(costh,m,qt,y,mode);
+//
+//  costh = -1.0; m = 110; qt = 20; y = -2.5;
+//  test_resum_speed(costh,m,qt,y,mode);
+//
+//  costh = 0.1; m = 91; qt = 5; y = 3.5;
+//  test_resum_speed(costh,m,qt,y,mode);
+//
+//  costh = 0.1; m = 91; qt = 5; y = 4.0;
+//  test_resum_speed(costh,m,qt,y,mode);
+//
+//  costh = 0.1; m = 91; qt = 5; y = 0.2;
+//  test_resum_speed(costh,m,qt,y,mode);
 
-  costh = 0.5; m = 70; qt = 10; y = 1.5;
-  test_resum_speed(costh,m,qt,y,mode);
-
-  costh = -1.0; m = 110; qt = 20; y = -2.5;
-  test_resum_speed(costh,m,qt,y,mode);
-
-  costh = 0.1; m = 91; qt = 5; y = 3.5;
-  test_resum_speed(costh,m,qt,y,mode);
-
-  costh = 0.1; m = 91; qt = 5; y = 4.0;
-  test_resum_speed(costh,m,qt,y,mode);
-
-  costh = 0.1; m = 91; qt = 5; y = 0.2;
-  test_resum_speed(costh,m,qt,y,mode);
-
-  costh = 0.1; m = 91; qt = 5; y = 0.2;
-  test_ct_speed(costh,m,qt,y,mode);
+//  costh = 0.1; m = 91; qt = 5; y = 0.2;
+//  test_ct_speed(costh,m,qt,y,mode);
 
   double f[2];
+  /*
   costh = 0.; m = opts.rmass; qt = 0; y = 0.; mode = 1;
   loint::lint(costh, m, y, mode, f);
   cout << "born cross section at m = " << m << " y = " << y << ": " << setprecision(12) << f[0]*2*M_PI*2*m/1000. << " pb" << endl;
@@ -116,32 +121,60 @@ int main( int argc , char * argv[])
   loint::lint(costh, m, y, mode, f);
   cout << "born cross section at m = " << m << " y = " << y << ": " << setprecision(12) << f[0]*2*M_PI*2*m/1000. << " pb" << endl;
 
-  costh = 0.; m = opts.rmass; qt = 0; y = 0; mode = 3;
-  opts.mellin1d = true;
-  //bool modlog = opts.modlog;
-  //int evmode = opts.evolmode;
-  //opts.modlog = true;
-  //opts.evolmode = 0;
-  f[0] = resint::rint(costh, m, qt, y, mode);
-  cout << "born cross section at m = " << m << ": " << f[0]/(8./3.)*2*m/1000. << endl;
-  //opts.modlog = modlog;
-  //opts.evolmode = evmode;
 
-  cout << endl << endl;
-  cout << "#qt" << "\t" << "dsigma / dqT" << endl;
+  costh = 0.; m = opts.rmass; qt = 0; y = 0; mode = 3;
+  //opts.mellin1d = true;
+  bool modlog = opts.modlog;
+  int evmode = opts.evolmode;
+  opts.modlog = true;
+  opts.evolmode = 0;
+  resint::rint(costh, m, qt, y, mode, f);
+  cout << "born cross section at m = " << m << ": " << f[0]/(8./3.)*2*m/1000. << endl;
+  opts.modlog = modlog;
+  opts.evolmode = evmode;
+  */
+
 
   double qtarr[52] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
 	       45,50,55,60,65,70,75,80,85,90,95,100};
-  scales::set(phasespace::m);
-  scales::mcfm();
-  opts.mellin1d = false;
+
+  /*
+  cout << endl << endl;
+  cout << "#qt" << "\t" << "dsigma / dqT" << endl;
+  //opts.mellin1d = false;
   for (int i = 0; i < 52; i++)
     {
       costh = 0.; m = opts.rmass; qt = qtarr[i]; y = 0; mode = 1;
-      double value = resint::rint(costh,m,qt,y,mode)/(8./3.)*2*m/1000;
+      phasespace::set_m(m);
+      phasespace::set_y(y);
+      phasespace::set_qt(qt);
+      scales::set(phasespace::m);
+      scales::mcfm();
+      phasespace::set_phiV(0.);
+      omegaintegr::genV4p();
+      resint::rint(costh,m,qt,y,mode,f);
+      double value = f[0]/(8./3.)*2*m/1000;
       cout << qt << "\t" << value << endl;
     }
+  */
 
+  /*
+  cout << endl << endl;
+  std::cout << std::setprecision(12);
+  cout << "#qt" << "\t" << "dsigma / dqT" << endl;
+  for (int i = 0; i < 52; i++)
+    {
+      costh = 0.; m = opts.rmass; qt = qtarr[i]; y = 0;
+      phasespace::set_mqtyphi(m, qt, y);//set global variables to costh, m, qt, y
+      phasespace::set_cth(costh);//set global variables to costh, m, qt, y
+      phasespace::calcexpy();
+      phasespace::calcmt();
+      omegaintegr::genV4p();//generate boson 4-momentum, with m, qt, y and phi=0
+      double vj = vjint::vint(m,qt,y);
+      cout << qt << "\t" << vj*2*m/1000 << endl;
+    }
+  */
+  
 //  cout << endl;
 //  cout << "#b" << "\t" << "S(b)" << endl;
 //  double value = resint::rint(costh,m,qt,y,mode)/(8./3.)*2*m/1000;
@@ -151,9 +184,36 @@ int main( int argc , char * argv[])
 //      complex <double> sudak=sudakov::sff(bb);
 //      cout << real(bb) << "\t" << real(sudak) << endl;
 //    }
+
+//  //plot the b-integrand
+//  costh = 0.; m = opts.rmass; qt = 0.5; y = 0; mode = 3;
+//  //double f[2];
+//  resint::rint(costh,m,qt,y,mode,f);
+//  cout << "{" << endl;
+//  cout << "TGraph *g = new TGraph();" << endl;
+//  double bmax = resint::bc/opts.bcf;
+//  for (int i = 0; i < 1000; i++)
+//    {
+//      double x = i/1000. * bmax * 2;
+//      complex <double> b;
+//      //      double b = b_mb*0.95+i*(b_mb*1.05 -b_mb*0.95)/100.;
+//      //double b = 0.+i*(b_mb*5 -0)/1000.;
+//      if (x < resint::bc)
+//	b = x;
+//      else
+//	{
+//	  complex <double> jacu = complex <double> (1.,tan(M_PI/opts.phibr));
+//	  b = resint::bc + jacu*(x-resint::bc);
+//	}
+//      //      cout << b << "  " << b_mb << "  " << besselint::bint(b) << endl;;
+//      cout << "g->SetPoint(g->GetN(), " << b << ", " << besselint::bint(b) << ");" << endl;
+//    }
+//  cout << "g->Draw();" << endl;
+//  cout << "}" << endl;
+  
   
   //costhline();
-  //ptline();
+  ptline();
   //ptomline();
   //yline();
   //mline();
@@ -162,6 +222,8 @@ int main( int argc , char * argv[])
   //ptavar();
   //ptgvar();
 
+  return 0;
+  
   /**************************************/
   //Checks for finite order cross section
   //born level variables (6 dimensions)
@@ -208,7 +270,9 @@ void test_resum_speed(double costh,double m,double qt,double y,int mode){
     cout << setw(10) << "Result" << setw(10) << "(fortran)" << setw(15) << value
          << setw(10) << "time "  << setw(15) << float(end_time - begin_time) << "s" << endl;
     begin_time = clock_real();
-    value = resint::rint(costh,m,qt,y,mode);
+    double f[opts.totpdf];
+    resint::rint(costh,m,qt,y,mode,f);
+    value = f[0];
     end_time = clock_real();
     cout << setw(10) << "Result" << setw(10) << "(C++)" << setw(15) << value
 	 << setw(10) << "time "  << setw(15) << float(end_time - begin_time) << "s" << endl;
