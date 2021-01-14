9929d32268915763d5fe868da3782104def1983b
Stefano Camarda <stefano.camarda@cern.ch>
2021-01-14 14:54:48 +0100

    split integrations, add logasl with different blim

--- resum/gint.h
+++ resum/gint.h
@@ -8,8 +8,9 @@ using namespace std;
 namespace gint
 {
   void init();
-  void intg(complex <double> q, complex<double> jac);
-  void intbeta(complex <double> q, complex <double> jac);
+  void intg(complex <double> q, complex<double> jac, double blim);
+  void intbeta(complex <double> q, complex <double> jac, double blim);
+  void intexpc(complex <double> q, complex <double> jac, double blim);
   complex <double> calc(complex <double> b);
   void allocate();
   void reset();
@@ -18,12 +19,14 @@ namespace gint
   extern complex <double> logS;
   
   extern complex <double> logasl;
+  extern complex <double> logasl_pdf;
+  extern complex <double> logasl_expc;
   extern complex <double> *alogqq;
   extern complex <double> *alogqg;
   extern complex <double> *alogqqb;
   extern complex <double> *alogqqp;
   extern complex <double> *alogqqbp;
-#pragma omp threadprivate(logS,logasl,alogqq,alogqg,alogqqb,alogqqp,alogqqbp)
+#pragma omp threadprivate(logS,logasl,logasl_pdf,logasl_expc,alogqq,alogqg,alogqqb,alogqqp,alogqqbp)
 }
 
 #endif
