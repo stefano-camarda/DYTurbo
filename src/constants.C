3dfb3b9eca2d6143ece9fad1208e66cce67bf02a
Stefano Camarda <stefano.camarda@cern.ch>
2021-01-21 14:38:49 +0100

    added li4half

--- src/constants.C
+++ src/constants.C
@@ -13,11 +13,12 @@ const double constants::CA = 3.;    //Casimir
 const double constants::euler = 0.57721566490153286061; //https://oeis.org/A001620
                                 
 //Values of the Riemann zeta function
-const double constants::zeta2 = 1.64493406684822643647; //M_PI*M_PI/6.;
-const double constants::zeta3 = 1.20205690315959428540; //https://oeis.org/A002117
-const double constants::zeta4 = 1.08232323371113819152; //https://oeis.org/A013662
-const double constants::zeta5 = 1.03692775514336992633; //https://oeis.org/A013663
-const double constants::zeta6 = 1.01734306198444913971; //https://oeis.org/A013664
+const double constants::zeta2   = 1.64493406684822643647; //M_PI*M_PI/6.;
+const double constants::zeta3   = 1.20205690315959428540; //https://oeis.org/A002117
+const double constants::zeta4   = 1.08232323371113819152; //https://oeis.org/A013662
+const double constants::zeta5   = 1.03692775514336992633; //https://oeis.org/A013663
+const double constants::zeta6   = 1.01734306198444913971; //https://oeis.org/A013664
+const double constants::li4half = 0.51747906167389938633; //https://arxiv.org/abs/hep-ph/9806280 Eq. (43)
 
 //Powers of Pi
 const double constants::pi  = M_PI;
